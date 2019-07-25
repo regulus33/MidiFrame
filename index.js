c09ac13b14e1cbb6103e5952af2c6f462714adab
Zack Rowden <zacharyrowden89@gmail.com>
2019-07-25 18:54:20 +0200

    record midi and receive and send request

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const bodyParser = require('body-parser');
+const app = express();
 var path = require('path');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 const port = 3000
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/midi.html')))
 
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+app.post('/midi',function(req,res){
+    const song = req.body 
+    console.log(song);
+    res.end("yes");
+  });
+
+
+const createVideo = (jsonMidi) => {
+    
+    //https://trac.ffmpeg.org/wiki/Concatenate
+    // async function ls() {
+    //   const { stdout, stderr } = await exec('ls');
+    //   console.log('stdout:', stdout);
+    //   console.log('stderr:', stderr);
+    // }
+    // ls();
+
+}
