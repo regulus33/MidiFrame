c4e7ba649cb98477fa37d286f62697809a047e62
regulus33 <zacharyrowden89@gmail.com>
2019-07-26 19:57:05 +0200

    Write json post to local fs.

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const exec = util.promisify(require('child_process').exec);
 const bodyParser = require('body-parser');
 const app = express();
 var path = require('path');
+const fs = require('fs').promises;
+
+// This must run inside a function marked `async`:
+
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -15,14 +19,25 @@ const port = 3000
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/midi.html')))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Listening on ${port}!`))
 
 
 app.post('/midi',function(req,res){
     const song = req.body 
     console.log(song);
+    console.log("writing song");
     res.end("yes");
-  });
+    // const file = await fs.readFile('filename.txt', 'utf8');
+   writeMidi(song)
+    console.log("file is written file")
+});
+
+
+async function writeMidi(midiJson) {
+    await fs.writeFile(path.join(__dirname + '/json/miditest.json'), JSON.stringify(midiJson));
+}
+
+
 
 
 const createVideo = (jsonMidi) => {
