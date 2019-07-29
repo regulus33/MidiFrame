e91ce60660a479d8b2b26091eeea2ad101494209
regulus33 <zacharyrowden89@gmail.com>
2019-07-30 00:22:20 +0200

    Pseudo coding next stepssss

--- genFilm.js
+++ genFilm.js
@@ -65,6 +65,18 @@ const sortedChannels = {
 }
 
 
+channelStartPoints = {
+    "1":2.45,
+    "2":3.67,
+    "3":6.78,    
+}
+
+channelVideos = {
+    "1":"test_a.mp4",
+    "2":"test_b.mp4",
+    "3":"test_b.mp4" 
+}
+
 
 midiFile = path.join(__dirname + '/json/miditest.json')
 
@@ -80,7 +92,29 @@ fs.readFile(midiFile, 'utf8', function(err, data) {
     fs.writeFileSync(path.join(__dirname + '/json/processed.json'), JSON.stringify(sortedChannels));
 
     sortedChannels["1"].forEach((e) => {
-        e.timeStamp[]
+        let start = channelStartPoints["1"]
+        let video = channelVideos["1"]
+         
+        // 1. GENERATE CLIPS
+        //choose 2:01 in video_pool/wood.mp4 
+        //this start point will remain the same for all clips, the only variable is the end point as the midi down will vary
+        //for each midi_off:
+        // subtract the midi_on timestamp at index - 1 in the array (it will always be preceded by its instigator) as LENGTH
+        // and split the video from 2:01 to LENGTH
+        // and generate folder name = temp_<titel>_<timestamp> 
+        // name each outputed file the startpoint at which to begin the video, ie, the note_on event that it belongs to's timestamp
+        
+        //2. HANDLE SILENCE 
+        //Silence can be thought of as the time between midi clips. 
+        // note_on: 1, note_off: 3,[HERE IS WHERE SILENCE IS 2 IN THIS CASE] note_on: 5, note_off: 7
+        //silence will be the difference between note_off to note_on
+        //repeat the above steps but instead of using wood, use empty.mp4 and set start time to 0.
+        //put the sliced files in the same directory as previos step used
+
+        //3. MERGE CLIP
+        //finally, sort all files by title in numerical order from smallest in a list
+        //print each file name to input.txt file, write it, run ffmpeg concat and wait :) 
+        
     })
 
 });
