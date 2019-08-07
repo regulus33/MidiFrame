16e5a9e4d1d19275872dea31f660f52fb74955a7
regulus33 <zacharyrowden89@gmail.com>
2019-08-07 23:39:44 +0200

    Refactoring into a standard rect - express app.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import singleChannel from './SingleChannel'
+import './App.css';
+import SingleChannel from './SingleChannel';
+
+function App() {
+  return (
+   <SingleChannel/>
+  );
+}
+
+export default App;
---
+++ src/SingleChannel.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import  SingleChannelHelper from './classes/SingleChannelHelper.js'
+import Recorder from 'recorderjs'
+
+const SingleChannel = (onClick) => {
+
+const helper = new SingleChannelHelper(document, window, Recorder)
+
+  return (
+    <div id="div" style={{width:'100%',height:'700px',backgroundColor:'cornflowerblue'}}>
+      <h1 style={{textAlign:"center", letterSpacing: "5px", fontFamily: "Arial, Helvetica, sans-serif", color: "white"}}>CLICK ANYWHERE TO BEGIN... THEN PRESS PLAY ON YOUR OPZ</h1>
+      <h1 id="iterableEmoji" style={{textAlign:"center", fontSize: "90px"}}>💀</h1>
+    </div>
+  );
+}
+
+export default SingleChannel
