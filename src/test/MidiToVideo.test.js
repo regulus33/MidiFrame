50df1b4dc1266a43aadb2bff32ae0c4a0b069741
Regulus33 <zacharyrowden89@gmail.com>
2019-09-11 18:32:11 +0200

    add new test for concat file generation just need to add the artray to text file and backend is basically done all tests passing

--- src/test/MidiToVideo.test.js
+++ src/test/MidiToVideo.test.js
@@ -139,24 +139,51 @@ test('generateChannelSliceCommands() returns array of valid ffmpeg commands', ()
 
 test('generateChannelSliceCommands() returns array of valid ffmpeg commands', () => {
     
+    let channel, notes, clip, data 
+
+    notes = {"55":"3:33", "54":"2:22", "53": "1:11", "52": '0:00'}
+    clip = "beethoven.mp4"
+    data = {
+        "7": [
+                {
+                    noteOn: true,
+                    timeStamp: 1,
+                    noteNumber: '55',
+                    velocityNumber: '100'
+                },
+            ]
+            
+    }
+
+    //FS SETUP
     let app_root = __dirname.split("/")
     app_root.pop()
     app_root.pop()
+    app_root = app_root.join('/')
 
     console.log(__dirname)
 
-    let args = {}
+    let fakeChannelDir = {}
 
-    args[`${app_root}/midi_slices/channel_2`] = {
-        "5.2323455" : "video file content",
-        "2.2325455" : "video file content",
-        "0.9898909" : "more video"
-
+    fakeChannelDir[`${app_root}/midi_slices/channel_2`] = {
+        "5.2323455.mp4" : "video file content",
+        "0.9898909.mp4" : "more video",
+        "2.2325455.mp4" : "video file content"
     }
 
-     mockFS(args);
+    let m = new MidiToVideo("2",notes,clip,data)
 
-     expect(fs.readdirSync(`${app_root}/midi_slices/channel_2`)[0]).toBe("ds")
+    //create a fake filesystem and overwrite it in memory 
+     mockFS(fakeChannelDir);
+     //make sure this node version is sorting the readdirsync results (yeah yeah i know)
+     expect(fs.readdirSync(`${app_root}/midi_slices/channel_2`)[2]).toBe("5.2323455.mp4")
+
+    let concatArray  = m.generateFfmpegConcatArgsForSelf()
+    let result =  "file" + " '" +`${app_root}/midi_slices/channel_2/5.2323455.mp4`+ "'" + "\n"
+
+    expect(concatArray[2]).toBe(result)
+
+
 })
 
 
