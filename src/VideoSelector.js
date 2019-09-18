67df1300d596198f2f713e6a4f633441ecbbef28
Regulus33 <zacharyrowden89@gmail.com>
2019-09-19 00:10:09 +0200

    Container components

--- src/VideoSelector.js
+++ src/VideoSelector.js
@@ -1,52 +1,16 @@
 import React from 'react'
-import { thisExpression } from '@babel/types';
 
 
-const fs = require('fs')
+const VideoSelecter = props => {
 
-
-class VideoSelecter extends React.Component {
-
-    constructor(props) {
-        super(props);
-        //for our array in assets s
-        this.state = {videoFiles:[]};
-        this.renderOptionsForDropDown = this.renderOptionsForDropDown.bind(this);
-    }
-
-    componentDidMount(){
-        this.fetchVideoFilePaths()
-    }
-
-    fetchVideoFilePaths() {
-        fetch('http://localhost:3000/video-selector').then(res => {
-            console.log(res)
-            res.json().then((r) => {
-                this.setState({videoFiles:r})
-            })
-        })
-    }
-
-    renderOptionsForDropDown() {
-        return this.state.videoFiles.map((address) => {
-            let nameOfSelectedVideo = address.split("/").pop()
-            return (
-                <option value={address}>{nameOfSelectedVideo}</option>
-            )
-        })
-
-    }
-
-    render() {
-        return (
-            <div>
-                <select>
-                    { this.renderOptionsForDropDown() }
-                </select>
-            </div>
-        )
-    }
-
+    return (
+        <div style={{ width: "100%", margin: "auto" }}>
+            <select>
+                {props.renderOptionsForDropDown()}
+            </select>
+        </div>
+    )
 }
+
 
 export default VideoSelecter
---
+++ src/containers/VideoSelectorContainer.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import VideoSelector from '../VideoSelector.js'
+
+class VideoSelecterContainer extends React.Component {
+
+    constructor(props) {
+        super(props);
+        //for our array in assets s
+        this.state = {videoFiles:[]};
+        this.renderOptionsForDropDown = this.renderOptionsForDropDown.bind(this);
+    }
+
+    componentDidMount(){
+        this.fetchVideoFilePaths()
+    }
+
+    fetchVideoFilePaths() {
+        fetch('http://localhost:3000/video-selector').then(res => {
+            console.log(res)
+            res.json().then((r) => {
+                this.setState({videoFiles:r})
+            })
+        })
+    }
+
+    renderOptionsForDropDown() {
+        return this.state.videoFiles.map((address) => {
+            let nameOfSelectedVideo = address.split("/").pop()
+            return (
+                <option value={address}>{nameOfSelectedVideo}</option>
+            )
+        })
+
+    }
+
+    render() {
+        return (
+          <VideoSelector renderOptionsForDropDown={this.renderOptionsForDropDown}/>
+        )
+    }
+
+}
+
+export default VideoSelecterContainer
