b7a19ef521d3b5754d1d36f6df8c9b138d0bb70f
Regulus33 <zacharyrowden89@gmail.com>
2019-09-26 00:52:12 +0200

    closer

--- src/test/VideoSelectorContainer.test.js
+++ src/test/VideoSelectorContainer.test.js
@@ -1,14 +1,13 @@
-/**
- * @jest-environment node
- */
+
 import VideoSelectorContainer from '../containers/VideoSelectorContainer'
+import VideoSelector from '../VideoSelector'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestRenderer from 'react-test-renderer';
 import util from 'util'
+import { exportAllDeclaration } from '@babel/types';
 // Test to get all students record
 it("video-selector returns an array of video assets", (done) => {
-
     const nock = require('nock');
     const request = require('request');
     
@@ -16,8 +15,10 @@ it("video-selector returns an array of video assets", (done) => {
     const path = '/video-selector';
 
     nock(baseURL)
+        .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
         .get(path)
-        .reply(200, 'ok');
+        .reply(200, JSON.stringify(["moomoo.moomoo.mp4"]))
+        .once(done())
     
     // request({
     //     uri: baseURL + path,
@@ -25,9 +26,11 @@ it("video-selector returns an array of video assets", (done) => {
 
     const renderer = TestRenderer.create(<VideoSelectorContainer/>)
     const instance = renderer.root 
+
+    expect(renderer.toJSON()).toBe(true)
     // console.log(util.inspect(instance))
     //onsole.log(util.inspect(instance._fiber.stateNode.state))
-    
+        
   
     
 
