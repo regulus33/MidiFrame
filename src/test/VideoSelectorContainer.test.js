a3b06ce85113f3dd41b6bab9523bb5a9a1556532
Regulus33 <zacharyrowden89@gmail.com>
2019-09-27 00:50:00 +0200

    SSuccess for testing containers, extract network

--- src/test/VideoSelectorContainer.test.js
+++ src/test/VideoSelectorContainer.test.js
@@ -6,32 +6,32 @@ import ReactDOM from 'react-dom';
 import TestRenderer from 'react-test-renderer';
 import util from 'util'
 import { exportAllDeclaration } from '@babel/types';
+import { promised } from 'q';
+global.fetch = require('jest-fetch-mock')
 // Test to get all students record
 it("video-selector returns an array of video assets", (done) => {
-    const nock = require('nock');
-    const request = require('request');
-    
-    const baseURL = 'http://localhost:3000';
-    const path = '/video-selector';
 
-    nock(baseURL)
-        .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
-        .get(path)
-        .reply(200, JSON.stringify(["moomoo.moomoo.mp4"]))
-        .once(done())
-    
-    // request({
-    //     uri: baseURL + path,
-    // }, console.log);
+    const moomoo = Promise.resolve(['jjj', 'pooppeep'])
 
-    const renderer = TestRenderer.create(<VideoSelectorContainer/>)
+    const shittyFetch = Promise.resolve(
+        {
+            json: () => moomoo
+        }
+    )
+
+    const videoSelectorGetMocked = jest.fn()
+    videoSelectorGetMocked.mockReturnValueOnce(shittyFetch)    
+
+    const renderer = TestRenderer.create(<VideoSelectorContainer videoSelectorGet={videoSelectorGetMocked} />)
     const instance = renderer.root 
 
-    expect(renderer.toJSON()).toBe(true)
-    // console.log(util.inspect(instance))
-    //onsole.log(util.inspect(instance._fiber.stateNode.state))
-        
-  
+    return Promise.allSettled([moomoo]).then(()=>{
+        expect(renderer.toJSON()).toBe(true)
+
+    })
+
+
+
     
 
 });
