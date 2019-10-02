6b6303548431a9dda662c63e78ab856514c5d52d
Regulus33 <zacharyrowden89@gmail.com>
2019-10-02 22:40:22 +0200

    Test channel pickeer

---
+++ src/test/ChannelPicker.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ChannelPicker from '../ChannelPicker.js';
+import ShallowRenderer from 'react-test-renderer/shallow';
+
+it('Channel Picker Renders props properly', () => {
+    let thingyToRenderrrrrrrr = "random rendered string, doesn't matter what it is as long as I am rendered insiiiiiiiide"
+    const fakePropsFunction = jest.fn()
+    fakePropsFunction.mockReturnValueOnce(thingyToRenderrrrrrrr)  
+    const renderer = new ShallowRenderer()
+    renderer.render(<ChannelPicker renderOptionsForDropDown={fakePropsFunction}/>)
+    const result = renderer.getRenderOutput();
+    expect(result.props.children).toEqual(<select>random rendered string, doesn't matter what it is as long as I am rendered insiiiiiiiide</select>)
+
+});
