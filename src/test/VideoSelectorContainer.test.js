68493303ade829d44300f99258615cc3bfa25ebb
Regulus33 <zacharyrowden89@gmail.com>
2019-11-07 19:03:48 +0100

    COver videoselectorcont in tests

--- src/test/VideoSelectorContainer.test.js
+++ src/test/VideoSelectorContainer.test.js
@@ -1,7 +1,6 @@
 import VideoSelectorContainer from '../containers/VideoSelectorContainer'
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
-import Option from '../Option.js'
 import BrowserMidiCollector from '../classes/BrowserMidiCollector';
 
 describe("VideoSelectorContainer",() => {
@@ -29,6 +28,7 @@ describe("VideoSelectorContainer",() => {
                 midiCollector={new BrowserMidiCollector()}
             />
         )
+
         return Promise.allSettled([innermostPromise, fakeVidFetch]).then(()=>{
             let instance = renderer.getInstance()
             instance.state.latestCapturedMidi = [{"data":["148","31","100"],"timeStamp":100}]
@@ -75,7 +75,131 @@ describe("VideoSelectorContainer",() => {
     })
 
     it("handleVideoOptionClick() sets the state of selected vid path",(done) => {
+        const innermostPromise = Promise.resolve(['path/to/video1.mp4', 'path/to/video.mp4'])
+        const fakeVidFetch = Promise.resolve(
+            {
+                json: () => innermostPromise
+            }
+        )
+        const videoSelectorGetMocked = jest.fn()
+        videoSelectorGetMocked.mockReturnValueOnce(fakeVidFetch)    
+        let mock = [{"data":["148","31","100"],"timeStamp":5254.274999955669}]
+        const renderer = TestRenderer.create(
+            <VideoSelectorContainer 
+                videoSelectorGet={videoSelectorGetMocked} 
+                rawMidi={mock} 
+                midiCollector={new BrowserMidiCollector()}
+            />
+        )
+        let instance = renderer.getInstance()
+        let mockEvent = {
+            target:{
+                selectedOptions:[{value:"fakeOptionValue"}]
+            }
+        }
+        instance.handleVideoOptionClick(mockEvent)
+        expect(instance.state.selectedVideoPath).toBe("fakeOptionValue")
+        return done()
+       
+            
+    })
 
+    it("handleTimeStampInput() updates the state.notes to something like: {'35': '2:22', '56': '3:33'}",(done) => {
+        const innermostPromise = Promise.resolve(['path/to/video1.mp4', 'path/to/video.mp4'])
+        const fakeVidFetch = Promise.resolve(
+            {
+                json: () => innermostPromise
+            }
+        )
+        const videoSelectorGetMocked = jest.fn()
+        videoSelectorGetMocked.mockReturnValueOnce(fakeVidFetch)    
+        let mock = [{"data":["148","31","100"],"timeStamp":5254.274999955669}]
+        const renderer = TestRenderer.create(
+            <VideoSelectorContainer 
+                videoSelectorGet={videoSelectorGetMocked} 
+                rawMidi={mock} 
+                midiCollector={new BrowserMidiCollector()}
+            />
+        )
+        let instance = renderer.getInstance()
+        instance.getTimeInputs = jest.fn() 
+        instance.getTimeInputs.mockReturnValueOnce([
+            {name: "56", value: "3:33"},
+            {name: "35", value: "2:22"},
+        ])
+        instance.handleTimeStampInput()
+        expect(instance.state.notes).toEqual({"35": "2:22", "56": "3:33"})
+        return done()
+            
+    })
+
+
+    it("handleChannelOptionClick() sets selected channel to state",(done) => {
+        const innermostPromise = Promise.resolve(['path/to/video1.mp4', 'path/to/video.mp4'])
+        const fakeVidFetch = Promise.resolve(
+            {
+                json: () => innermostPromise
+            }
+        )
+        const videoSelectorGetMocked = jest.fn()
+        videoSelectorGetMocked.mockReturnValueOnce(fakeVidFetch)    
+        let mock = [{"data":["148","31","100"],"timeStamp":5254.274999955669}]
+        const renderer = TestRenderer.create(
+            <VideoSelectorContainer 
+                videoSelectorGet={videoSelectorGetMocked} 
+                rawMidi={mock} 
+                midiCollector={new BrowserMidiCollector()}
+            />
+        )
+        let instance = renderer.getInstance()
+        let mockEvent = {
+            target:{
+                selectedOptions: [
+                    {value:"69"}
+                ]
+            }
+        }
+
+        instance.handleChannelOptionClick(mockEvent)
+
+        expect(instance.state.selectedChannel).toBe("69")
+
+        return done()
+            
+    })
+
+    it("renderOptionsForChannelPickerData() returns options for all used channels", (done) => {
+        const innermostPromise = Promise.resolve(['path/to/video1.mp4', 'path/to/video.mp4'])
+        const fakeVidFetch = Promise.resolve(
+            {
+                json: () => innermostPromise
+            }
+        )
+        const videoSelectorGetMocked = jest.fn()
+        videoSelectorGetMocked.mockReturnValueOnce(fakeVidFetch)    
+        let mock = [
+            {"data":["148","31","100"],"timeStamp":5254.274999955669},
+            {"data":["157","31","100"],"timeStamp":5254.274999955669},
+            {"data":["153","31","100"],"timeStamp":5254.274999955669},
+            {"data":["148","31","100"],"timeStamp":5254.274999955669},
+            {"data":["148","31","100"],"timeStamp":5254.274999955669},
+        ]
+        const renderer = TestRenderer.create(
+            <VideoSelectorContainer 
+                videoSelectorGet={videoSelectorGetMocked} 
+                rawMidi={mock} 
+                midiCollector={new BrowserMidiCollector()}
+            />
+        )
+        let instance = renderer.getInstance()
+        instance.state.latestCapturedMidi = mock
+        //convert the react array to array of channel strings for the test
+        expect(instance.renderOptionsForChannelPickerData().map((r)=>r.props.value)).toEqual(["5", "10", "14"])    
+        return done()
+            
+    })
+
+    it("renderOptionsForVideoDropDown() should pass AND refresh updates sstate properly ", (done) => {
         const innermostPromise = Promise.resolve(['path/to/video1.mp4', 'path/to/video.mp4'])
 
         const fakeVidFetch = Promise.resolve(
@@ -89,6 +213,7 @@ describe("VideoSelectorContainer",() => {
         let mock = [{"data":["148","31","100"],"timeStamp":5254.274999955669}]
 
 
+
         const renderer = TestRenderer.create(
             <VideoSelectorContainer 
                 videoSelectorGet={videoSelectorGetMocked} 
@@ -96,56 +221,23 @@ describe("VideoSelectorContainer",() => {
                 midiCollector={new BrowserMidiCollector()}
             />
         )
-
-        let instance = renderer.getInstance()
-
-        let mockEvent = {
-            target:{
-                selectedOptions:[{value:"fakeOptionValue"}]
-            }
-        }
-        instance.handleVideoOptionClick(mockEvent)
-
-        expect(instance.state.selectedVideoPath).toBe("fakeOptionValue")
-
-        return Promise.allSettled([innermostPromise, fakeVidFetch]).then(()=>{
-            let instance = renderer.getInstance()
-            instance.state.latestCapturedMidi = [{"data":["148","31","100"],"timeStamp":100}]
-            instance.state.selectedChannel = "5"
-            expect(instance.getUsedNotesObject()).toEqual({
-                "5": [31]
-            })
-            done() 
-        })
-            
-    })
-
-    // it("video-selector returns an array of video assets", (done) => {
-
-    //     const renderer = TestRenderer.create(
-    //         <VideoSelectorContainer 
-    //             videoSelectorGet={videoSelectorGetMocked} 
-    //             rawMidi={mock} 
-    //             midiCollector={new BrowserMidiCollector()}
-    //         />
-    //     )
-    //     const testInstance = renderer.root;
         
 
-    //     return Promise.allSettled([innermostPromise, fakeVidFetch]).then(()=>{
-    //         debugger
-    //         //first option picker is channel 
-    //         console.log(testInstance.findAllByType(Option))
-    //         // expect(testInstance.findAllByType(Option)[0].props.value).toBe("5")
-    //         //second is video path 
-    //         // expect(testInstance.findAllByType(Option)[1].props.value).toBe("path/to/video1.mp4")
-    //         done()
+        return Promise.allSettled([innermostPromise, fakeVidFetch]).then(()=>{
+
+            let instance = renderer.getInstance()
+            expect(renderer.getInstance().renderOptionsForVideoDropDown()[0].props.value).toBe("path/to/video1.mp4")
+            
+            
+            //TODO call refresh here and update browsermidicollector first 
+            
+            return done()
 
 
-    //     })
+        })
 
 
-    // });
+    });
 
 })
 
