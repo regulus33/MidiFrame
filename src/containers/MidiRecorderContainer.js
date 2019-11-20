c32251464e2222856daf22e2d0ec800c7f52aa07
Regulus33 <zacharyrowden89@gmail.com>
2019-11-20 13:14:52 +0100

    begin midi recorder

---
+++ src/containers/MidiRecorderContainer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default class MidiRecorderContainer extends React.Component  {
+    constructor(props){
+        super(props)
+
+    }
+
+    componentDidMount(){
+        
+    }
+
+    render(){
+        return(
+            <h1>Midi Recorder</h1>
+          
+        )
+    }
+}
