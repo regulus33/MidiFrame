69e0514a17954f01995912bf20eb5a379116580b
zack <zacharyrowden89@gmail.com>
2019-11-24 12:47:24 +0100

    scrubbing incoming, seems to be working but needs refactor and to be more verbose

--- server.js
+++ server.js
@@ -52,12 +52,18 @@ app.post('/midi',(req,res) => {
 
     console.log('\x1b[36m%s\x1b[0m', 'POST /midi'); 
     // const dataObject = JSON.parse(req.body)
-    debugger 
-    Object.keys(req.body.metaData).forEach((key)=>{
-      const dataObject = req.body.metaData[key]
-      if(dataObject[key].videoPath != '' &&  dataObject[key].notes != ''){
+    Object.keys(req.body.metaData).forEach((channelKey)=>{
+      const dataObject = req.body.metaData[channelKey]
+      debugger 
+      if(dataObject[channelKey] != undefined && channelKey != '' && dataObject[channelKey].videoPath != '' && dataObject[channelKey].notes != ''){
         //loop through each channel, for each one, instantiate the video class and run the code for that channel to be generate 
         //when done, take a shit
+        const v = new MidiToVideo(req.body.musicData)
+        v.makeClips() 
+        v.createInput()
+        let pathToInputTextFile =  path.join(__dirname, `/midi_slices/input_${channelKey}`) 
+        execSync(`ffmpeg concat ${pathToInputTextFile}`)
+
       }
     })
     
