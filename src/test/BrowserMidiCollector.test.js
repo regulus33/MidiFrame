ebf855c05b41f3b0ef39ef9325c1ad9adc037994
Regulus33 <zacharyrowden89@gmail.com>
2019-11-29 23:49:13 -0500

    tests for delete

--- src/test/BrowserMidiCollector.test.js
+++ src/test/BrowserMidiCollector.test.js
@@ -336,6 +336,43 @@ it("storeMidiDataInLocalStorage() stores midiData plus old data in local storage
 
 })
 
+    deleteMidiDataFromLocalStorage(name = null, window = window) {
+//TODOOOOO test 
+it("deleteMidiDataFromLocalStorage(), pass", () => {
+    let mockWindow = {
+        localStorage: {
+            removeItem: jest.fn(),
+        }
+    }
+
+
+
+    const b = new BrowserMidiCollector()
+    
+    b.midiData = {
+        "1":{notes: [1,2,3], videoPath: "path.mp4"},
+        "2":{notes: [1,2,3], videoPath: "path.mp4"},
+        "3":{notes: [1,2,3], videoPath: "path.mp4"},
+        "4":{notes: [1,2,3], videoPath: "path.mp4"},
+        "5":{notes: [1,2,3], videoPath: "path.mp4"},
+        "6":{notes: [1,2,3], videoPath: "path.mp4"},
+        "7":{notes: [1,2,3], videoPath: "path.mp4"},
+        "8":{notes: [1,2,3], videoPath: "path.mp4"},
+        "9":{notes: [1,2,3], videoPath: "path.mp4"},
+        "10":{notes: [1,2,3], videoPath: "path.mp4"}
+    }
+
+    let mockReturnOfremoveItem = [b.formatMidiDataForStorage(),b.formatMidiDataForStorage(),b.formatMidiDataForStorage(),b.formatMidiDataForStorage(),b.formatMidiDataForStorage(),b.formatMidiDataForStorage()]
+    mockWindow.localStorage.removeItem.mockReturnValue(JSON.stringify(mockReturnOfremoveItem))
+    mockReturnOfremoveItem.push(b.formatMidiDataForStorage("fpoon"))
+    let expectedSecondArg = mockReturnOfremoveItem
+
+    b.storeMidiDataInLocalStorage("fpoon", mockWindow)
+
+    expect(mockWindow.localStorage.setItem).toHaveBeenLastCalledWith('opz-app',JSON.stringify(expectedSecondArg) )
+
+})
+
 
 it("Saves mididata that was loaded ", () => {
     let mockWindow = {
