1fade60009c5dda782888062403cff5ad78dc5f5
Regulus33 <zacharyrowden89@gmail.com>
2019-12-01 23:31:26 -0500

    fix midi breaking when incomplete input

--- src/test/MidiPlayerLive.test.js
+++ src/test/MidiPlayerLive.test.js
@@ -26,5 +26,33 @@ it("Plays video at time paired to this channel", () => {
 })
 
 
+it("doesn't play when a non-number is passsed", () => {
+
+    let midiEvent = {"data":["148","31","100"],"timeStamp":5254.274999955669}
+    let state = {
+        selectedChannel: "5",
+        notes: {    
+            31: ":30",
+            32: "3:30",
+        }
+    }
+
+    MidiPlayerLive.playVideoAtSecondsStart = jest.fn() 
+   
+    MidiPlayerLive.playNote(148, 31, state.selectedChannel, state.notes)
+    expect(MidiPlayerLive.playVideoAtSecondsStart).not.toHaveBeenCalled()
+
+})
+
+
+it("makeSureInputIsNumber() should pass", () => {
+    expect(MidiPlayerLive.makeSureInputIsNumber("3:33")).toBe(true)
+    expect(MidiPlayerLive.makeSureInputIsNumber(":33")).toBe(false)
+    expect(MidiPlayerLive.makeSureInputIsNumber("3:")).toBe(false)
+    expect(MidiPlayerLive.makeSureInputIsNumber(":30")).toBe(false)
+
+})
+
+
 
 
