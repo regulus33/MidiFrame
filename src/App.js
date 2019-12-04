55b3b57324b74e581a7399a1f5665f8d77f56d76
Regulus33 <zacharyrowden89@gmail.com>
2019-12-03 22:51:08 -0500

    make buttonss in videoselectorcontainer reuseable

--- src/App.js
+++ src/App.js
@@ -46,6 +46,8 @@ class App extends React.Component {
   }
 
   determineAppState(){
+    return "SHOW_MAIN_APP"
+
     if(this.state.showProjectManager === true){
       return "SHOW_PROJECT_MANAGER"
     }
