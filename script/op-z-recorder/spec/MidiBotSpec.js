fea5691f8b00b6884c6cece3c755eaaca08d28a4
zack <zacharyrowden89@gmail.com>
2020-02-09 19:12:19 +0100

    add jasmine

---
+++ script/op-z-recorder/spec/MidiBotSpec.js
@@ -0,0 +1,53 @@
+describe("MIDI BOT", function() {
+  var player;
+  var song;
+
+  beforeEach(function() {
+    let newThing = 0 
+  });
+
+  it("should be a test", function() {
+    expect(3).toEqual(3);
+  });
+
+  // describe("when song has been paused", function() {
+  //   beforeEach(function() {
+  //     player.play(song);
+  //     player.pause();
+  //   });
+
+  //   it("should indicate that the song is currently paused", function() {
+  //     expect(false).toBeFalsy();
+
+  //     // demonstrates use of 'not' with a custom matcher
+  //     expect(player).not.toBePlaying(song);
+  //   });
+
+  //   it("should be possible to resume", function() {
+  //     player.resume();
+  //     expect(player.isPlaying).toBeTruthy();
+  //     expect(player.currentlyPlayingSong).toEqual(song);
+  //   });
+  // });
+
+  // // demonstrates use of spies to intercept and test method calls
+  // it("tells the current song if the user has made it a favorite", function() {
+  //   spyOn(song, 'persistFavoriteStatus');
+
+  //   player.play(song);
+  //   player.makeFavorite();
+
+  //   expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
+  // });
+
+  //demonstrates use of expected exceptions
+  // describe("#resume", function() {
+  //   it("should throw an exception if song is already playing", function() {
+  //     player.play(song);
+  //     calcClockSignalsBeforeDone(30000)
+  //     expect(function() {
+  //       player.resume();
+  //     }).toThrowError("song is already playing");
+  //   });
+  // });
+});
