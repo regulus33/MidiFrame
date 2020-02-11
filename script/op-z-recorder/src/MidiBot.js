1c1bcf00e3953f5c355d71f69942af7eb0e47fc3
zack <zacharyrowden89@gmail.com>
2020-02-11 23:04:46 +0100

    starting the tests

--- script/op-z-recorder/src/MidiBot.js
+++ script/op-z-recorder/src/MidiBot.js
@@ -47,140 +47,153 @@
         return CLOCK_SIGNALS_IN_1_BAR * barCount
     }
  
-    const patternCountSlider = document.getElementById("patternCountRange");
-    const patternCountSliderOutput = document.getElementById("patternCount");
-    const progressBar = document.getElementById("progress")
-    patternCountSliderOutput.innerHTML = patternCountSlider.value;
-    progressBar.max = calcClockSignalsBeforeDone(patternCountSlider.value)
+    // const patternCountSlider = document.getElementById("patternCountRange");
+    // const patternCountSliderOutput = document.getElementById("patternCount");
+    // const progressBar = document.getElementById("progress")
+    // patternCountSliderOutput.innerHTML = patternCountSlider.value;
+    // progressBar.max = calcClockSignalsBeforeDone(patternCountSlider.value)
 
-    patternCountSlider.oninput = () => {
-        const totalBars = patternCountSlider.value
-        patternCountSliderOutput.innerHTML = totalBars
-        clockSignalsBeforeDone = calcClockSignalsBeforeDone(totalBars)
-        progressBar.max = clockSignalsBeforeDone
-    }
+    // patternCountSlider.oninput = () => {
+    //     const totalBars = patternCountSlider.value
+    //     patternCountSliderOutput.innerHTML = totalBars
+    //     clockSignalsBeforeDone = calcClockSignalsBeforeDone(totalBars)
+    //     progressBar.max = clockSignalsBeforeDone
+    // }
 
-    /////////project sliders 
-    const projectSlotSlider = document.getElementById("projectSlotRange");
-    const projectSlotSliderOutput = document.getElementById("projectSlot");
-    projectSlotSliderOutput.innerHTML = projectSlotSlider.value;
-    selectedProject = projectSlotSlider.value 
+    // /////////project sliders 
+    // const projectSlotSlider = document.getElementById("projectSlotRange");
+    // const projectSlotSliderOutput = document.getElementById("projectSlot");
+    // projectSlotSliderOutput.innerHTML = projectSlotSlider.value;
+    // selectedProject = projectSlotSlider.value 
 
-    projectSlotSlider.oninput = () => {
-        const projectSlotValue = projectSlotSlider.value
-        projectSlotSliderOutput.innerHTML = projectSlotValue
-        selectedProject = projectSlotValue
-    }
+    // projectSlotSlider.oninput = () => {
+    //     const projectSlotValue = projectSlotSlider.value
+    //     projectSlotSliderOutput.innerHTML = projectSlotValue
+    //     selectedProject = projectSlotValue
+    // }
 
 
-    const midiConsent = (event) => {
-        console.log('requesting to midi access')
-        request()
-    } 
+    // const midiConsent = (event) => {
+    //     console.log('requesting to midi access')
+    //     request()
+    // } 
 
-    async function cycleMidi() {
-        //fill the array if it isnt full yet, first time 
-        if(tracksToRecord.length === 0) {
-           assignRecordTracks()
-        }
+    // async function cycleMidi() {
+    //     //fill the array if it isnt full yet, first time 
+    //     if(tracksToRecord.length === 0) {
+    //        assignRecordTracks()
+    //     }
 
-        let currentTrackToRecord = tracksToRecord.pop() 
-       //iterate through every patthern an mute all but selected
-       for(let i = 0; i < 15; i++) {
-            //go through each pattern and mute tracks
-            await sleep(DELAY) 
-            // sendMessage([PROJECTS_TO_CODES[selectedProject],103,i])
-            sendMessage([183,103,16])
-            //mute all but current 
-            Object.keys(MUTE_COMMANDS).forEach((trackName) => {
-                if( trackName != currentTrackToRecord ) {
-                    // sendMuteMessages(trackName)
-                }
-            })
+    // //     let currentTrackToRecord = tracksToRecord.pop() 
+    // //    //iterate through every patthern an mute all but selected
+    // //    for(let i = 0; i < 15; i++) {
+    // //         //go through each pattern and mute tracks
+    // //         await sleep(DELAY) 
+    // //         sendMessage([PROJECTS_TO_CODES[selectedProject],103,i])
+    // //         //mute all but current 
+    // //         Object.keys(MUTE_COMMANDS).forEach((trackName) => {
+    // //             if( trackName != currentTrackToRecord ) {
+    // //                 // sendMuteMessages(trackName)
+    // //             }
+    // //         })
 
-       } 
-        //all  mutes have been applied, start at pattern 0 
-        // sendMessage([250])
-        // recording = true 
+    // //    } 
+    //     //all  mutes have been applied, start at pattern 0 
+    //     let currentTrackToRecord = tracksToRecord[0]
+    //     Object.keys(MUTE_COMMANDS).forEach((trackName) => {
+    //         if( trackName != currentTrackToRecord ) {
+    //             sendMuteMessages(trackName)
+    //         }
+    //     })
+    //     recording = true 
+    //     sendMessage([250])
         
-    }
+    // }
 
-    async function sendMuteMessages(trackName){
-        let muteMessage = MUTE_COMMANDS[trackName]
-        console.log("sending: " + muteMessage )
-        await sleep(DELAY)
-        sendMessage(muteMessage)
-    }
+    // async function sendMuteMessages(trackName){
+    //     let muteMessage = MUTE_COMMANDS[trackName]
+    //     console.log("sending: " + muteMessage )
+    //     await sleep(DELAY)
+    //     sendMessage(muteMessage)
+    // }
 
-    const assignRecordTracks = () => {
-        const tracks = document.getElementsByClassName("tracks")
+    // const assignRecordTracks = () => {
+    //     const tracks = document.getElementsByClassName("tracks")
 
-        for(let i = 0; i < tracks.length; i++) {
-            let element = tracks[i]
-            if(element.checked) {
-                tracksToRecord.push(element.value)
-            } 
-        }
+    //     for(let i = 0; i < tracks.length; i++) {
+    //         let element = tracks[i]
+    //         if(element.checked) {
+    //             tracksToRecord.push(element.value)
+    //         } 
+    //     }
       
-        // these and then store them somewhere than iterate through that 
-        // once user hits start 
-        // then record mono and if you can do 2 channels record each chanel of each track 
-    }
+    //     // these and then store them somewhere than iterate through that 
+    //     // once user hits start 
+    //     // then record mono and if you can do 2 channels record each chanel of each track 
+    // }
 
 
-    const request = () => {
-        return navigator.requestMIDIAccess( {sysex: true }).then( access => {
-            midiAccess = access
-            const inputPorts = access.inputs.values();
-            const outputPorts = access.outputs.values();
-            for (let out of outputPorts) {
-                deviceId = out.id 
-            }
-            for (let device of inputPorts ) {
-                if (device.name == "OP-Z") {
-                    console.log(device.name + " connected")
-                    device.onmidimessage = onMidiMessage 
-                    // sendMessage("nothing")
-                } else {
-                    alert("Can't find an OP-Z :( Make Sure Your OP-Z is plugged into to the usb port on your device.")
-                }
-            }
-        }).catch(console.error);
-    }
+    // const request = () => {
+    //     return navigator.requestMIDIAccess( {sysex: true }).then( access => {
+    //         midiAccess = access
+    //         const inputPorts = access.inputs.values();
+    //         const outputPorts = access.outputs.values();
+    //         for (let out of outputPorts) {
+    //             deviceId = out.id 
+    //         }
+    //         for (let device of inputPorts ) {
+    //             if (device.name == "OP-Z") {
+    //                 console.log(device.name + " connected")
+    //                 device.onmidimessage = onMidiMessage 
+    //                 // sendMessage("nothing")
+    //             } else {
+    //                 alert("Can't find an OP-Z :( Make Sure Your OP-Z is plugged into to the usb port on your device.")
+    //             }
+    //         }
+    //     }).catch(console.error);
+    // }
 
-    const onMidiMessage = (message) => {
-        //IF THIS IS THE CLOCK SIGNAL AND WE ARE IN RECORDING MODE 
-        if(recording && message.data[0] == 248) {
-            //IF WE HAVE REACHED THE END OF THE PROJECT
-            if(clockCount > 0 && clockCount == clockSignalsBeforeDone){
-                //stop op-z 
-                alert('length finished')
-                //if there are stil tracks to record we will recall cycleMidi()
-                if(tracksToRecord.length === 0) {
-                    alert('recording is done')
-                    sendMessage([252])
-                } else {
-                    cycleMidi()
-                }
-            } else {
-                clockCount += 1 
-                progressBar.value = clockCount
-            }
-        } 
+    // const onMidiMessage = (message) => {
+    //     //IF THIS IS THE CLOCK SIGNAL AND WE ARE IN RECORDING MODE 
+    //     if(recording && message.data[0] == 248) {
+    //         debugger 
+    //         let currentTrackToRecord = tracksToRecord[0]
+    //         Object.keys(MUTE_COMMANDS).forEach((trackName) => {
+    //             if( trackName != currentTrackToRecord ) {
+    //                 debugger 
+    //                 sendMuteMessages(trackName)
+    //             }
+    //         })
+    //         //IF WE HAVE REACHED THE END OF THE PROJECT
+    //         if(clockCount > 0 && clockCount == clockSignalsBeforeDone){
+    //             //stop op-z 
+    //             alert('length finished')
+    //             //if there are stil tracks to record we will recall cycleMidi()
+    //             if(tracksToRecord.length === 0) {
+    //                 alert('recording is done')
+    //                 sendMessage([252])
+    //             } else {
+    //                 cycleMidi()
+    //             }
+    //         } else {
+    //             clockCount += 1 
+    //             progressBar.value = clockCount
+    //         }
+    //     } 
       
-    }
+    // }
 
     const sendMessage = (message) => { 
         const output = midiAccess.outputs.get(deviceId)
         output.send(message)
     }
 
-    const sleep = (ms) => {
-        return new Promise(resolve => setTimeout(resolve, ms))
-    }
+    // const sleep = (ms) => {
+    //     return new Promise(resolve => setTimeout(resolve, ms))
+    // }
 
-    document.getElementById('midiConsent').onclick = midiConsent 
-    // document.getElementById('start').onclick = test
-    document.getElementById('start').onclick = cycleMidi 
+    // document.getElementById('midiConsent').onclick = midiConsent 
+    // // document.getElementById('start').onclick = test
+    // document.getElementById('start').onclick = cycleMidi 
 
 
