02f7b48ef52f030352c71b5ffe9ae50ddc30ee84
zack <zacharyrowden89@gmail.com>
2020-03-01 17:51:25 +0100

    add react rails

--- midiframe/app/controllers/projects_controller.rb
+++ midiframe/app/controllers/projects_controller.rb
@@ -1,67 +1,63 @@
+# frozen_string_literal: true
+# Nest all patterns associated with a single video here 
 class ProjectsController < ApplicationController
 
-    before_action :get_project, only: [:edit, :update, :destroy]
+  before_action :get_project, only: [:edit, :update, :destroy]
 
-    def index 
-        @projects = current_user.projects 
-        redirect_to new_project_path if @projects.empty? 
+  def edit; end
+
+  def new 
+    @project = Project.new(user: current_user, bpm: 120)
+  end
+
+  def update
+    @project.bpm = project_params[:bpm].to_i 
+    @project.name = project_params[:name] 
+    @project.video = project_params[:video] 
+
+    if @project.save 
+      flash[:notice] = "Post successfully created"
+      render 'index'
+    else 
+    # TODO: handle form validation 
+      render 'layouts/error'
     end 
+  end 
 
-    def edit 
+  def create 
+    @project = Project.new 
+    @project.bpm = project_params[:bpm].to_i 
+    @project.name = project_params[:name] 
+    @project.video = project_params[:video] 
+    @project.user = current_user
+
+    if @project.save 
+      flash[:notice] = "Post successfully created"
+        render 'index'
+    else 
+      # TODO: handle form validation stuff
+      render 'layouts/error'
     end 
+  end 
 
-    def new 
-        @project = Project.new(user: current_user, bpm: 120)
-    end 
+  def destroy 
+    @deleted_name = @project.name
+    if(@project.destroy)
+      render 'index'
+    else 
+      render 'layouts/error'
+    end             
+  end 
 
-    def update
-        @project.bpm = project_params[:bpm].to_i 
-        @project.name = project_params[:name] 
-        @project.video = project_params[:video] 
+  private 
 
-        if @project.save 
-            flash[:notice] = "Post successfully created"
-            render 'index'
-        else 
-            #TODO: handle form validation stuff
-            render 'layouts/error'
-        end 
-    end 
+  def project_params
+    params.require(:project).permit(:name, :bpm, :video)
+  end  
 
-    def create 
-        @project = Project.new 
-        @project.bpm = project_params[:bpm].to_i 
-        @project.name = project_params[:name] 
-        @project.video = project_params[:video] 
-        @project.user = current_user
-
-        if @project.save 
-            flash[:notice] = "Post successfully created"
-            render 'index'
-        else 
-            #TODO: handle form validation stuff
-            render 'layouts/error'
-        end 
-    end 
-
-    def destroy 
-        @deleted_name = @project.name
-        if(@project.destroy)
-            render 'index'
-        else 
-            render 'layouts/error'
-        end             
-    end 
-
-    private 
-
-    def project_params
-        params.require(:project).permit(:name, :bpm, :video)
-    end  
-
-    def get_project
-        @project = Project.find_by(id: params[:id])
-    end
+  def get_project
+    @project = Project.find_by(id: params[:id])
+  end
 
 
 end
--- midiframe/config/routes.rb
+++ midiframe/config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
   #PROJECTS 
   root to: 'projects#index'
 
-  resources :projects
-  
+  resources :projects do  
+    resources :patterns 
+  end 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
