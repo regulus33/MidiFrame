075b7271f6c7b43e8decd4e89611a1e5b31adc43
zack <zacharyrowden89@gmail.com>
2020-03-02 08:55:31 +0100

    readme

---
+++ midiframe/app/javascript/components/PatternEditor.js
@@ -0,0 +1,14 @@
+import React from "react"
+import PropTypes from "prop-types"
+class PatternEditor extends React.Component {
+
+  render () {
+    return (
+      <React.Fragment>
+        
+      </React.Fragment>
+    );
+  }
+}
+
+export default PatternEditor
