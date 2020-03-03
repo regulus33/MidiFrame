9d52b83a4bc674082ac7e47e6926b2c0abc5615d
zack <zacharyrowden89@gmail.com>
2020-03-03 14:34:01 +0100

    midi handlers

--- midiframe/app/javascript/projects.js
+++ midiframe/app/javascript/projects.js
@@ -1,45 +1,45 @@
 
-// const { 
-  // request_midi_access_from_chrome,
-  // midi_message_jamming,
-// } = require('./shared.js');
+const { 
+  requestMidiAccessFromChrome,
+  onMidiMessageJamming,
+  onRejectedAccessFromChrome
+} = require('./shared.js');
 
-window.addEventListener('load', (event) => {
-    ///////////////////////////////////////////////
-    //                                           //
-    //   VIDEO UPLOAD INPUT BUTTON IS SO UGLY,   //
-    //    THIS HIDES IT AND DISPLAYS FILENAME    //
-    //          IN AN ARBITRARY SPAN             //
-    //                                           //
-    ///////////////////////////////////////////////
-    const file_input_tag = document.getElementById("project_video");
 
-    const fil_name_displayer_span = document.getElementById("video_file_name");
+// Got to wait for dom to load
+window.onload = (event) => {
 
-    file_input_tag.addEventListener('change', (event) => {
+    const fileInputTag = document.getElementById("project_video");
+    const fileNameDisplayerSpan = document.getElementById("video_file_name");
+    const newPatternButton = document.getElementById('newPatternButton');
+    
+    // Make file input button display video filename with out own styles 
+    fileInputTag.addEventListener('change', (event) => {
         //split path and pop off the filename only:
-        fil_name_displayer_span.innerText = event.target.value.split('\\').pop();  
+        fileNameDisplayerSpan.innerText = event.target.value.split('\\').pop();  
     });
-    //Before the user navigates to the pattern page, we need to get their midi consent. 
-    //To do this we can bind the permission function to the new project button. 
-    //we'll prevent the default event (naving) and request Chrome's midi access. Once we get access we nav the user to the page 
-    //if not, we throw alert telling them to plug in their device 
-    const new_pattern_button = document.getElementById('new_pattern_button')
-    new_pattern_button.onclick = (event) => {
-      event.preventDefault();
-      //passing the request to the API the callback I want to trigger each time the midi device sends us a MASSAGE 
-      //it altimately returns false, but inside of a gosh dang PROMISE. Promises to keep am i right?
-      //Is that all we're fuckin doin heah? Prowmissing>? PROOOOOWWWMMMISSSING>>>>>???? 
-      request_midi_access_from_chrome(midi_message_jamming).then((resolved_promise)=>{
-        
-      });
-      
-   
-    }
 
+    // Request midi access before navigating to pattern editor
+    new_pattern_button.onclick = (event) => {
+     
+      event.preventDefault();
+
+      requestMidiAccessFromChrome({
+        //once we get access call our function that will nav us to the pattern page but give them the a tag to call click on
+        onReceiveAccessFromChrome : () => navigateToPatternEditor(event.target),
+        onReceiveMidiMessage: onMidiMessageJamming, 
+        onRejectedAccessFromChrome, 
+
+      });
+    }
+    //I only added this handler in this file because all the other handlers being passed
+    //to requestMidiAccessFromChrome are generic and re-useable 
+    const navigateToPatternEditor = (anchor) => {
+      debugger 
+      anchor.click();
+    }
     
-    
-});
+};
 
 
 
