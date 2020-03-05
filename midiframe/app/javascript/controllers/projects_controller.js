004548069ad69970aff2de03447196310efb32a8
zack <zacharyrowden89@gmail.com>
2020-03-06 00:59:21 +0100

    tests and toast

--- midiframe/app/javascript/controllers/projects_controller.js
+++ midiframe/app/javascript/controllers/projects_controller.js
@@ -5,13 +5,10 @@ export default class extends Controller {
   static targets = [ "filename", "fileDisplayName" ]
 
   connect(){
-
     alert('dddd')
   }
 
   printFileName() {
-    
-    debugger 
     this.fileDisplayNameTarget.innerText = this.filenameTarget.value.split("\\")[this.filenameTarget.value.split("\\").length - 1]
   }
 }
