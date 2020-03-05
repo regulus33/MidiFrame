3cf1956f0288bce7d5f687c2b44dbdfa57b347f0
zack <zacharyrowden89@gmail.com>
2020-03-06 00:59:21 +0100

    tests and toast

--- midiframe/test/controllers/projects_controller_test.rb
+++ midiframe/test/controllers/projects_controller_test.rb
@@ -3,7 +3,30 @@
 require 'test_helper'
 
 class ProjectsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "new, should pass" do
+
+    get '/projects/new'
+
+    assert_response :success 
+
+  end
+
+  test "create, should pass" do
+    post '/projects', params: { project: { bpm: 120, name: 'new project'} }
+
+    assert_response :success
+
+  end
+
+  test "delete, should pass" do
+    project = projects(:one)
+    
+    assert_difference('Project.count', -1) do
+      delete project_path( project ) 
+
+      assert_response :redirect
+    end
+
+  end
+
 end
