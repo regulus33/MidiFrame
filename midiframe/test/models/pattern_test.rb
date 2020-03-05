259f5196643fdeb9405923ae68711196e8c1bc3e
Regulus33 <zacharyrowden89@gmail.com>
2020-03-05 17:44:31 +0100

    sliding keyboard

--- midiframe/test/models/pattern_test.rb
+++ midiframe/test/models/pattern_test.rb
@@ -3,7 +3,7 @@
 require 'test_helper'
 
 class PatternTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "NOTES_GROUPED_IN_OCTAVES" do
+    assert Pattern::NOTES_GROUPED_IN_OCTAVES.length == 9
+  end
 end
