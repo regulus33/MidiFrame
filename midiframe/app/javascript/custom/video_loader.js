599d9cd48c58e5204bdb816de5698e8d2e202cf3
zack <zacharyrowden89@gmail.com>
2020-03-10 02:29:32 +0100

    async await and progress bar nearly finished.

--- midiframe/app/javascript/custom/video_loader.js
+++ midiframe/app/javascript/custom/video_loader.js
@@ -1,40 +1,63 @@
 import videojs from 'video.js'
                           //video controller nows about the video tag and passes a callback 
-export const waitForVideoToLoad = (videoTagId, onDownloaded) => {
+export const waitForVideoToLoad = async (videoTagId, onDownloaded) => {
   let myPlayer = videojs(videoTagId)
-  fetchWholeVideo(myPlayer)
-  // /// Wait until player is fully loaded to play()
-  myPlayer.on("progress", () => {
-
-    if(myPlayer.bufferedPercent() > 5){
-      onDownloaded(myPlayer.bufferedPercent())
-    }
-  })
-}
-
-// JUST DEFAULT A LOADING BAR TIL THIS IS COMPLETE 
-// TODO CACHEING WITH OR WITHOUT SERVICE WORKER. 
-// EVENTUALLY WE CAN DO PARTIAL CONTENT. ADAPTIVELY LOAD THE VIDEO AND MAKE THE TIMELINE LONGER AS THE VIDEO GROWS
-// AGAIN! THATS A STRETCH GOAL
-const fetchWholeVideo = (myPlayer) => {
-  // const req = new XMLHttpRequest()
-  //FIX ME 
+  
   let source = "http://localhost:3000" + myPlayer.src()
   
-  fetch(source)
-    .then(result => result.blob())
-    .then(blob => {
-      const blobURL = URL.createObjectURL(blob);
-    
-      // Doesn't work
-      // myPlayer.src(blobURL);
-      debugger
-      // Works
-      myPlayer.src({ src: blobURL, type: "video/mp4" });
-    }
-  )
+  const response = await fetch(source)
 
+  const blob = await response.blob()
+
+  const blobURL = URL.createObjectURL(blob)
+
+  myPlayer.src({ src: blobURL, type: "video/mp4" });
+  
+  showProgress(response, onDownloaded)
+  
 }
+//ALMOST THERE!!! PROBLEM IS JUST THAT SHOWPROGRESS IS SYNC FROM DOWNLOAD
+//COPY THIS https://javascript.info/fetch-progress
+const showProgress = async(response, onDownloaded) => {
+  const reader = response.body.getReader();
+  const contentLength = +response.headers.get('Content-Length');
+  // read the data
+  let receivedLength = 0; // received that many bytes at the moment
+  while(true) {
+    const {done, value} = await reader.read()
+    if (done) {
+      break
+    }
+    receivedLength += value.length;
+    console.log(`Received ${receivedLength} of ${contentLength}`)
+    let percentDownloaded = Math.floor(( receivedLength / contentLength ) * 100 )
+    console.log(percentDownloaded)
+    onDownloaded(percentDownloaded)
+  }
+  
+}
+
+
+
+// const fetchWholeVideo = (myPlayer) => {
+//   // const req = new XMLHttpRequest()
+//   //FIX ME 
+//   let source = "http://localhost:3000" + myPlayer.src()
+  
+//   let response = fetch(source)
+//     .then(result => result.blob())
+//     .then(blob => {
+//       const blobURL = URL.createObjectURL(blob);
+    
+//       // Doesn't work
+//       // myPlayer.src(blobURL);
+//       debugger
+//       // Works
+//       myPlayer.src({ src: blobURL, type: "video/mp4" });
+//     }
+//   )
+//    debugger
+// }
 
 
 
