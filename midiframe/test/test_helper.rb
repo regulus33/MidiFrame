9e2389d60b9c272e42df7a86a88b670df1456802
zack <zacharyrowden89@gmail.com>
2020-03-25 03:00:12 +0100

    integration testing starts now

--- midiframe/test/test_helper.rb
+++ midiframe/test/test_helper.rb
@@ -4,6 +4,8 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 require 'capybara/rails'
+require 'capybara/minitest'
+
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
@@ -12,5 +14,23 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+
   # Add more helper methods to be used by all tests here...
+end
+
+
+class ActionDispatch::IntegrationTest
+  # Make the Capybara DSL available in all integration tests
+  include Capybara::DSL
+  # Make `assert_*` methods behave like Minitest assertions
+  include Capybara::Minitest::Assertions
+
+  Capybara.default_driver = :selenium_chrome
+
+
+  # Reset sessions and driver between tests
+  teardown do
+    Capybara.reset_sessions!
+    Capybara.use_default_driver
+  end
 end
