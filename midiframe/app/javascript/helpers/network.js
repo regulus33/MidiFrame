974c8511cb57a8c44e3b62116eb2956e4e57f78f
zack <zacharyrowden89@gmail.com>
2020-03-29 01:40:00 +0100

    patch pattern

--- midiframe/app/javascript/helpers/network.js
+++ midiframe/app/javascript/helpers/network.js
@@ -1,13 +1,15 @@
-import { patternsUrl } from "./constants";
+import { baseUrl } from "./constants";
 
-export async const saveProject = (data = {}) => {
+export const patternsUrl = (projectId, patternId) => `${baseUrl}/projects/${projectId}/patterns/${patternId}`
+
+export const saveProject = async(data = {}, patternId, projectId) => {
   // Default options are marked with *
-  const response = await fetch(patternsUrl, {
-    method: 'PATCH',
+  const response = await fetch( patternsUrl(projectId, patternId), {
+    method: 'PUT',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
-  });
-  return await response.json(); // parses JSON response into native JavaScript objects
+    body: JSON.stringify( data ) // body data type must match "Content-Type" header
+  })
+  return await response.json() // parses JSON response into native JavaScript objects
 }
 
 
