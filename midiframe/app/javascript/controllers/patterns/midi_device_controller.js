b56ba1031f26baa2d3c465ff424ee9a6bcec4814
zack <zacharyrowden89@gmail.com>
2020-04-14 22:36:44 +0200

    cleaning up midi device controller

--- midiframe/app/javascript/controllers/patterns/midi_device_controller.js
+++ midiframe/app/javascript/controllers/patterns/midi_device_controller.js
@@ -19,40 +19,64 @@ export default class extends Controller {
     // ? just the notes used and the timestamps they should trigger in the video 
     // * {36: 5666} (note number: video timestamp)
     this.pianoData = {}
-    
     // ? keep track of the notes that come out of the device. 
     // * [{note: 60, timestamp: 3}]
-    this.midiEvents= [] 
-
     this.recording = false 
-
+    this.midiEvents = [] 
     this.startingTime = null 
-
     this.clockSignalsPassedSinceRecordStart = 0 
-
     this.video = videojs(this.videoTarget.id)
-
     this.selectedKey = null 
-
     this._observe_all_keys()
-   
     this._enable_midi() 
-   
     this._onVideoSeek = this.updateSelectedNoteTime.bind(this)
-
     this.saveAndNavigate = this.saveAndNavigate.bind(this)
-
     this._addKeyDownChannelListener()
-
     // * INITIALIZING PIANO DATA IF IN THE DOM 
     this._initializePianoData()
 
   }
 
-  /*
-  * @param web midi message api object
-  * runs whatever needs ot happen on midi message
-  */
+  //SAVE BUTTON 
+  save() {
+    return saveProject({channel: this._channel, pianoData: this.pianoData, midiEvents: this.midiEvents, patternId: this._getPatternId(), projectId: this._getProjectId()})
+    .then(() =>  M.toast( { html:'Pattern Saved'} ) )
+  }
+
+  saveAndNavigate() {
+    // console.log(this._settingsUrl)
+    this.save().then(() => { 
+      // console.log(this._settingsUrl)
+      window.location.href = baseUrl + this._settingsUrl
+    })
+  }
+
+
+  //? submitting data to be converted into a video 
+  generatePatternClip(){
+    // ! save before we tell controller to generate 
+    this.save().then((e) => {
+      generatePatternClip({patternId: this._getPatternId(), projectId: this._getProjectId()}).then(()=> {
+      })
+    })
+  }
+
+  onMessageClock(message) {
+    // ? only count clock signals if recording 
+    if(this._recording) {
+      this.clockSignalsPassedSinceRecordStart++
+      console.log(`clock signal passed: ${this.clockSignalsPassedSinceRecordStart} total clock signals: ${this._totaClockSignals}`)
+      switch(this.clockSignalsPassedSinceRecordStart) {
+        // ! end is reached! exit recording loop
+        case this._totaClockSignals:
+          this._addStopTime(message.timestamp)
+          this._recording = false 
+          break;
+      }
+    } 
+  }
+
+
   onMessageNoteOn(msg) {
     this._play_note(msg)
     this._play_video(msg)
@@ -61,6 +85,11 @@ export default class extends Controller {
 
   onMessageNoteOff(msg) {
     this._unplay_note(msg)
+  }
+
+  onMessageStart(msg) {
+    this._addStartEvent(msg.timestamp)
+    this._startRecordingMidiNotes()
   }
 
   onPianoKeyClick(event){
@@ -73,21 +102,11 @@ export default class extends Controller {
     let filteredTime = toTheNearestThousandth( event.target.player.currentTime() )
     if(this._selectedKey) {
      
-      this.updateData({time: filteredTime, number: this._selectedKey.id })
+      this._updateData({time: filteredTime, number: this._selectedKey.id })
       this._selectedKey.value = filteredTime
     }
   }
   
-  // ? needs to be refactored 
-  // 1. from seeking in the video 
-  // 2. nudging the timestamp back  
-  // 3. nudging the timestamp fourth   
-  // 4. randomizing
-  // TODO find a way to simplify this duplication  
-  updateData({time, number}) {
-    this.pianoData[number] = time 
-  }
-
   onDocumentKeyDown(e) {
     if(this._keyCodeIsNumber(e.key))  this._changeChannel(e.key)
   }
@@ -106,6 +125,18 @@ export default class extends Controller {
       }
   }
 
+  // **************************************************
+  // ! PRIVATE METHODS PRIVATE METHODS PRIVATE METHODS
+  // *************************************************
+
+  _startRecordingMidiNotes() {
+    this._recording = true 
+  }
+
+  _updateData({time, number}) {
+    this.pianoData[number] = time 
+  }
+
   // TODO: i think the object may need data to be converted i.e. integers 
   _initializePianoData(){
     let noteStamps = JSON.parse(this.noteStampsTarget.getAttribute("note-stamps"))
@@ -113,61 +144,6 @@ export default class extends Controller {
       this.pianoData = noteStamps
     }
 
-  }
-
-  get _playing(){
-    return this.playing
-  }
-
-  set _playing(playing){
-    this.playing = playing
-  }
-
-
-  set _onVideoSeek(fun){
-    this._video.on('seeking', (e) => fun(e))
-  }
-
-  set _selectedKey(element){
-    if(this.selectedKey) {
-      this._deactivatePianoKey(this.selectedKey)
-    } 
-    this._activatePianoKey(element)
-    this.selectedKey = element
-  }
-
-  get _selectedKey() {
-    return this.selectedKey
-  }
-
-  get _video(){
-    return this.video 
-  }
-
-  get _midiInput(){
-    return WebMidi.inputs[0]
-  }
-
-  get _midiOutput(){
-    return WebMidi.outputs[0]
-  }
-
-  get _piano(){
-    return this.piano
-  }
-
-
-  get _videoLength(){
-    return this._video.duration()
-  }
-
-  get _channel(){
-    return parseInt(this.channelTarget.getAttribute('device-channel'))
-  }
-
-  set _channel(channel) {
-    this.channelTarget.innerHTML = channel 
-    this.channelTarget.setAttribute('device-channel', channel)
   }
 
   _addKeyDownChannelListener(){
@@ -191,7 +167,7 @@ export default class extends Controller {
     let time = this._numericalValue(element.value)
     time += NUDGE_AMOUNT
     time = toTheNearestThousandth(time) 
-    this.updateData({time: time, number: element.id})
+    this._updateData({time: time, number: element.id})
     element.value = time 
   }
 
@@ -199,13 +175,13 @@ export default class extends Controller {
     let time = this._numericalValue(element.value)
     let nextTime = toTheNearestThousandth(time - NUDGE_AMOUNT)
     time = nextTime < 0 ? 0 : nextTime
-    this.updateData({time: time, number: element.id})
+    this._updateData({time: time, number: element.id})
     element.value = time 
   }
 
   _randomize(element){
     let randomValue = randoMize(this._videoLength)
-    this.updateData({time: randomValue, number: element.id})
+    this._updateData({time: randomValue, number: element.id})
     element.value = randomValue 
   } 
 
@@ -327,26 +303,7 @@ export default class extends Controller {
     this._midiInput.addListener('noteon', channel, msg => this.onMessageNoteOn(msg))
     this._midiInput.addListener('noteoff', channel, msg => this.onMessageNoteOff(msg))
     this._midiInput.addListener('clock', "all", this.onMessageClock.bind(this))
-  }
-
-  //SAVE BUTTON 
-  save() {
-    // console.log(this._getPatternId())
-    // console.log(this._getProjectId())
-    return saveProject({channel: this._channel, pianoData: this.pianoData, midiEvents: this.midiEvents, patternId: this._getPatternId(), projectId: this._getProjectId()})
-    .then(() =>  M.toast( { html:'Pattern Saved'} ) )
-  }
-
-  saveAndNavigate() {
-    // console.log(this.settingsUrl)
-    this.save().then(() => { 
-      // console.log(this.settingsUrl)
-      window.location.href = baseUrl + this.settingsUrl
-    })
-  }
-
-  get settingsUrl(){
-    return this.settingsTarget.getAttribute("nav-url")
+    this._midiInput.addListener('start', "all", this.onMessageStart.bind(this))
   }
 
   _getPatternId() {
@@ -357,87 +314,13 @@ export default class extends Controller {
     return this.projectIdTarget.getAttribute("project-id")
   }
 
-  //Midi Event Collection  Methods 
-  // setter and getter methods for midi events collection
-  
-  get _midiEvents() {
-    return this.midiEvents
-  }
-
-  startRecordingMidiNotes() {
-    // ? start playing
-    // * turn recording on 
-    // * increment clock signals in another process 
-    // * soon as clock signals reaches this.data.get("total-clock-signals") hits, exit by setting recording to false and
-    // * meanwhile, we only push data into array if recording is true    
-    //! recording also starts the midi when set to true
-    this._recording = true 
-
-  }
-
-  // ! HEADS UP:::::: there are two parts to this process, this only determines when to end recording time........
-  // ! SEE ON MESSAGE NOTE ON for the bit that saves data into an array to send to server  
-  // ? you need to increment the clock signals count(clock signals passed since record start), dont worry! It will be reset when recording stops 
-  onMessageClock(message) {
-    // ? only count clock signals if recording 
-    if(this._recording) {
-
-      this.clockSignalsPassedSinceRecordStart++
-      console.log(`clock signal passed: ${this.clockSignalsPassedSinceRecordStart} total clock signals: ${this._totaClockSignals}`)
-      
-      switch(this.clockSignalsPassedSinceRecordStart) {
-        // ! end is reached! exit recording loop
-        case this._totaClockSignals:
-          this._addStopTime(message.timestamp)
-          this._recording = false 
-          break;
-        case 1:
-          this._addStartEventAndSetStartTime(message.timestamp)
-          break;
-      }
-
-   
-
-    } 
-  }
-
-  // ? set recording AND also stop or start midi based on value of 'recording'
-  set _recording(recording) {
-    if(recording) {
-      this.recording = true
-      this._startMidi()
-    } else {
-      this.recording = false 
-      this._stopMidi()
-      // this._resetStartTime()
-    }
-  }
-
-  get _recording() {
-    return this.recording 
-  }
-
-  _startMidi(){
-    this._midiOutput.sendStart()
-  }
-
   _stopMidi(){
     this._midiOutput.sendStop()
   }
 
-  _calibrateTiming(timestamp, startingTime) {
-    return timestamp - startingTime 
-  }
-
-  get _totaClockSignals() {
-    return parseInt(this.recordButtonTarget.getAttribute("total-clock-signals"))
-  }
-
-  _addStartEventAndSetStartTime(timestamp) {
+  _addStartEvent(timestamp) {
     // ? on first clock signal of recording session we get the received time of the 
     // ? first note 
-   
-    this._startingTime = timestamp
     let processeableEvent = { note: "start", timestamp: timestamp }
     this._midiEvents.push(processeableEvent)
   }
@@ -450,43 +333,113 @@ export default class extends Controller {
   _addMidiEvent(event) {
     let calibratedTimeStamp 
     // ? first you need to get the amount of time to subtract from each timestamp so that the first evetn starts at 0:00
-    // if(this._midiEvents.length == 0) {
-    //   this._startingTime = event.timestamp
-    // }
     // ? set the timing in the new event  
-    // calibratedTimeStamp = this._calibrateTiming(event.timestamp, this._startingTime)
-    
     if(this._recording) {
       let processeableEvent = { note: event.note.number, timestamp: event.timestamp }
       this._midiEvents.push(processeableEvent)
     }
   }
-
-  get _startingTime(){
-    return this.startingTime 
+  
+  // **************************************************************
+  // ******************* GETTERS AND SETTERS **********************
+  // **************************************************************
+  
+  // ? CHANNEL
+  // ? we get it from the document, it is save in the pattern 
+  ///////////////////////////////////////////////////////////////////
+  get _channel(){
+    return parseInt(this.channelTarget.getAttribute('device-channel'))
+  }
+  set _channel(channel) {
+    this.channelTarget.innerHTML = channel 
+    this.channelTarget.setAttribute('device-channel', channel)
+  }
+  ////////////////////////////
+  ///////////////////////////
+  get _midiEvents() {
+    return this.midiEvents
+  }
+  ///////////////////////////
+  get _midiInput(){
+    return WebMidi.inputs[0]
   }
 
-  set _startingTime(time){
-    this.startingTime = time
+  get _midiOutput(){
+    return WebMidi.outputs[0]
   }
 
-  // ? this isnt necessary but clarifies that a new start time will be created for every new recording session
-  resetStartTime() {
-    this.startingTime = 0 
+  get _piano(){
+    return this.piano
   }
 
+  // ? PLAYING we use this to light up channel, to notify forms to ignore playhead's value 
+  // ? and probably a multitude of other things as new requirements emerge. 
+  /////////////////////////
+  get _playing(){
+    return this.playing
+  }
 
-  //? submitting data to be converted into a video 
-  generatePatternClip(){
-    // ! save before we tell controller to generate 
-    this.save().then((e) => {
-      debugger
-      generatePatternClip({patternId: this._getPatternId(), projectId: this._getProjectId()}).then(()=> {
-        debugger 
-      })
-    })
+  set _playing(playing){
+    this.playing = playing
+  }
+  /////////////////////////
+
+  // ? RECORDING 
+  // ? the SETTER has extrac functionality tied to it 
+  // ? that extra functionality is key to capturing midi events
+  // ? when we are in a record loop  
+  ///////////////////////////////////////////////////////////////////////
+  get _recording() {
+    return this.recording 
+  }
+  // ? set recording AND also stop or start midi based on value of 'recording'
+  set _recording(recording) {
+    if(recording) {
+      this.recording = true
+    } else {
+      this.recording = false 
+      this._stopMidi()
+    }
+  }
+  ////////////////////////////////////////////////////////////////////////
+  // ? SELECTED KEY 
+  // ? get the key (form input) that we need to change the time on 
+  get _selectedKey() {
+    return this.selectedKey
+  }
+  // ? set the key we will be performing for input changes on 
+  set _selectedKey(element){
+    if(this.selectedKey) {
+      this._deactivatePianoKey(this.selectedKey)
+    } 
+    this._activatePianoKey(element)
+    this.selectedKey = element
+  }
+  //////////////////////////////////////////////////////
+  // ? which url to navigate to update pattern 
+  get _settingsUrl(){
+    return this.settingsTarget.getAttribute("nav-url")
   }
   
+  // ? pre-calculated (on server side) total clock signals before a record session ends 
+  get _totaClockSignals() {
+    return parseInt(this.recordButtonTarget.getAttribute("total-clock-signals"))
+  }
+
+  // ? return the video element 
+  get _video(){
+    return this.video 
+  }
+
+  // ? used to calculate a random timestamp within video length range 
+  get _videoLength(){
+    return this._video.duration()
+  }
+
+  // ? set the function that will run on video seek, right now it updates the form 
+  set _onVideoSeek(fun){
+    this._video.on('seeking', (e) => fun(e))
+  }
 
 }
 
