774bd9c3bf4d1d978eeaa8d77dcb81b7f10c0ecc
zack <zacharyrowden89@gmail.com>
2020-04-22 01:43:32 +0200

    the concatting, still working on it

--- midiframe/app/models/ff_mpeg.rb
+++ midiframe/app/models/ff_mpeg.rb
@@ -28,10 +28,14 @@ class FfMpeg < ApplicationRecord
   # ? for each event in the @pattern.note_stamps array, this method is fed an event   
   # ? the method will output a string that is meant to be executed as a system command 
   # ? which invokes the FFMPEG binary for clip slicing. 
-  def generate_blue_print(event:, next_event:)
-    # ?              ffmpeg -t requires seconds, its still very precise so we just convert 
+  def generate_slice_blue_print(event:, next_event:)
+    # ? ffmpeg -t requires seconds, its still very precise so we just convert 
     slice_duration = milliseconds_to_seconds(next_event["timestamp"] - event["timestamp"])
     "ffmpeg -an -y -ss #{timestamp_to_play_in_video event: event} -i #{self.project_tempfile_url} -t #{slice_duration} -c:v libx264 #{generate_unique_tempfile_clip_location_url(event["timestamp"])}"
+  end
+
+  def generate_concat_blueprint(event)
+    "file '#{generate_unique_tempfile_clip_location_url(event["timestamp"])}'\n"
   end
 
   def timestamp_to_play_in_video(event:)
@@ -49,14 +53,18 @@ class FfMpeg < ApplicationRecord
     project = self.pattern.project.id 
     # ! TODO: security/stability make this below hash thing based on user email to ensure uniqueness
     # project-id--pattern-id--event-time should be unique enough so just don't forget to check this/test before production
-    "#{Rails.root}/tmp/#{project}-#{pattern}-#{time_stamp}.mp4"
+    "#{Rails.root}/tmp/#{project}-#{pattern}-#{remove_dots(time_stamp)}.mp4"
   end 
 
   # ? add the command to the blueprints array, we save it in this table to make it possible to run it 
   # ? in separate process for flexibility 
-  def add_a_command_to_pattern_blue_prints_array(blue_print) 
-    self.pattern_blueprints = [] if self.pattern_blueprints.nil?   
-    self.pattern_blueprints << blue_print
+  def add_commands_to_pattern_blue_prints(blue_prints) 
+    self.pattern_blueprints = blue_prints 
+    self.save!
+  end
+
+  def add_commands_to_pattern_concat_blue_prints(concat_blue_prints) 
+    self.pattern_concat_blueprints = concat_blue_prints 
     self.save!
   end
 
@@ -64,21 +72,32 @@ class FfMpeg < ApplicationRecord
   # ? we then save each of these commands to be run later 
   def loop_through_events_and_process_them(events:)     
     # ? for each event, get the start time and end time of a note by finding the event's timestamp
+    pattern_blueprints = []  
+    pattern_concat_blueprints = ""
     events.each_with_index do |event, index|
       # ? stop is the last event, there is not command for this event, it only serves as a sign when the last note should terminate
       break if event["note"] == "stop"
       # ? calculate the end time of the note by searching starttime of next note 
       next_event = events[index + 1] unless reached_the_last_event? current_index: index, array_length: events.length
       # ? build the string to be run / insertted in out blueprints collection 
-      blue_print_to_add = generate_blue_print(event: event, next_event: next_event)
+      blue_print_to_add = generate_slice_blue_print(event: event, next_event: next_event)
+      concat_blue_print_to_add = generate_concat_blueprint(event)
+      # ? COLLECT BLUEPRINTS FOR SLICE AND CONCAT 
+      pattern_blueprints << blue_print_to_add
+      pattern_concat_blueprints << concat_blue_print_to_add
       # insert the command 
-      add_a_command_to_pattern_blue_prints_array(blue_print_to_add)
       
     end
+    add_commands_to_pattern_blue_prints(pattern_blueprints)
+    add_commands_to_pattern_concat_blue_prints(pattern_concat_blueprints)
   end 
 
   def reached_the_last_event?(current_index:, array_length:)
     current_index + 1 == array_length 
   end 
+
+  def remove_dots(string)
+    string.to_s.gsub(".","")
+  end
 
 end
--- midiframe/db/schema.rb
+++ midiframe/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_12_135534) do
+ActiveRecord::Schema.define(version: 2020_04_21_185148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2020_04_12_135534) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "project_tempfile_url"
     t.string "processed_tempfile_url"
+    t.jsonb "pattern_concat_blueprints"
     t.index ["pattern_id"], name: "index_ff_mpegs_on_pattern_id"
   end
 
@@ -90,6 +91,7 @@ ActiveRecord::Schema.define(version: 2020_04_12_135534) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+    t.boolean "sound_stripped"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
