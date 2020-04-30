fd5669a6d7608e3526ff521718b955c7c1a68790
zack <zacharyrowden89@gmail.com>
2020-04-30 10:01:24 +0200

    fuck that flutter bullshit

--- midiframe/config/application.rb
+++ midiframe/config/application.rb
@@ -12,6 +12,14 @@ module Midiframe
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    # !just a workaround to get around cors error for dev
+    # ? https://medium.com/@Nicholson85/handling-cors-issues-in-your-rails-api-120dfbcb8a24
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+         origins '*'
+         resource '*', :headers => :any, :methods => [:get, :post, :options]
+       end
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -19,3 +27,5 @@ module Midiframe
     # the framework and any gems in your application.
   end
 end
+
+
