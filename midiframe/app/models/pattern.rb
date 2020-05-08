d0fb3e258708b36b343fb42e2f0c4248a4f3ac5d
zack <zacharyrowden89@gmail.com>
2020-05-08 23:52:48 +0200

    fixing file extension in pattern

--- midiframe/app/models/pattern.rb
+++ midiframe/app/models/pattern.rb
@@ -5,28 +5,25 @@ class Pattern < ApplicationRecord
   # ? name of midi notes signaling absolute beginning and end of a clip 
   START = "start"
   STOP = "stop"
-
   # ? To be joined as one entire project 
-  has_one_attached :video
-
+  has_one_attached :clip
   belongs_to :project
 
   before_create :set_channel, :initialize_name , :initialize_step_length , :initialize_note_stamps , :initialize_order_in_sequence
-
   # ? after create since the values set above must be present 
   after_create :set_channel, :initialize_name 
  
   # * all note numbers that we will support, normally there are 0-128 but Id rather keep things as simple as possible and ,ake the end result evenly divisible by twelve, which is how many notes we keep in our on screen keyboard 
   NOTES_GROUPED_IN_OCTAVES = 
   [
-    [[0, "C0"],  [1, "C#0"],  [2, "D0"],  [3, "D#0"],  [4, "E0"],  [5, "F0"],  [6, "F#0"],  [7, "G0"],  [8, "G#0"],  [9, "A0"],  [10, "A#0"], [11, "B0"]],
-    [[12, "C1"], [13, "C#1"], [14, "D1"], [15, "D#1"], [16, "E1"], [17, "F1"], [18, "F#1"], [19, "G1"], [20, "G#1"], [21, "A1"], [22, "A#1"], [23, "B1"]],
-    [[24, "C2"], [25, "C#2"], [26, "D2"], [27, "D#2"], [28, "E2"], [29, "F2"], [30, "F#2"], [31, "G2"], [32, "G#2"], [33, "A2"], [34, "A#2"], [35, "B2"]],
-    [[36, "C3"], [37, "C#3"], [38, "D3"], [39, "D#3"], [40, "E3"], [41, "F3"], [42, "F#3"], [43, "G3"], [44, "G#3"], [45, "A3"], [46, "A#3"], [47, "B3"]],
-    [[48, "C4"], [49, "C#4"], [50, "D4"], [51, "D#4"], [52, "E4"], [53, "F4"], [54, "F#4"], [55, "G4"], [56, "G#4"], [57, "A4"], [58, "A#4"], [59, "B4"]],
-    [[60, "C5"], [61, "C#5"], [62, "D5"], [63, "D#5"], [64, "E5"], [65, "F5"], [66, "F#5"], [67, "G5"], [68, "G#5"], [69, "A5"], [70, "A#5"], [71, "B5"]],
-    [[72, "C6"], [73, "C#6"], [74, "D6"], [75, "D#6"], [76, "E6"], [77, "F6"], [78, "F#6"], [79, "G6"], [80, "G#6"], [81, "A6"], [82, "A#6"], [83, "B6"]],
-    [[84, "C7"], [85, "C#7"], [86, "D7"], [87, "D#7"], [88, "E7"], [89, "F7"], [90, "F#7"], [91, "G7"], [92, "G#7"], [93, "A7"], [94, "A#7"], [95, "B7"]],
+    [[0, "C0"],  [1, "C#0"],  [2, "D0"],  [3, "D#0"],  [4, "E0"],  [5, "F0"],  [6, "F#0"],  [7, "G0"],  [8, "G#0"], [9, "A0"],  [10, "A#0"], [11, "B0"]],
+    [[12, "C1"], [13, "C#1"], [14, "D1"], [15, "D#1"], [16, "E1"], [17, "F1"], [18, "F#1"], [19, "G1"], [20, "G#1"],[21, "A1"], [22, "A#1"], [23, "B1"]],
+    [[24, "C2"], [25, "C#2"], [26, "D2"], [27, "D#2"], [28, "E2"], [29, "F2"], [30, "F#2"], [31, "G2"], [32, "G#2"],[33, "A2"], [34, "A#2"], [35, "B2"]],
+    [[36, "C3"], [37, "C#3"], [38, "D3"], [39, "D#3"], [40, "E3"], [41, "F3"], [42, "F#3"], [43, "G3"], [44, "G#3"],[45, "A3"], [46, "A#3"], [47, "B3"]],
+    [[48, "C4"], [49, "C#4"], [50, "D4"], [51, "D#4"], [52, "E4"], [53, "F4"], [54, "F#4"], [55, "G4"], [56, "G#4"],[57, "A4"], [58, "A#4"], [59, "B4"]],
+    [[60, "C5"], [61, "C#5"], [62, "D5"], [63, "D#5"], [64, "E5"], [65, "F5"], [66, "F#5"], [67, "G5"], [68, "G#5"],[69, "A5"], [70, "A#5"], [71, "B5"]],
+    [[72, "C6"], [73, "C#6"], [74, "D6"], [75, "D#6"], [76, "E6"], [77, "F6"], [78, "F#6"], [79, "G6"], [80, "G#6"],[81, "A6"], [82, "A#6"], [83, "B6"]],
+    [[84, "C7"], [85, "C#7"], [86, "D7"], [87, "D#7"], [88, "E7"], [89, "F7"], [90, "F#7"], [91, "G7"], [92, "G#7"],[93, "A7"], [94, "A#7"], [95, "B7"]],
     [[96, "C8"], [97, "C#8"], [98, "D8"], [99, "D#8"], [100, "E8"],[101, "F8"],[102, "F#8"],[103, "G8"],[104, "G#8"],[105, "A8"],[106, "A#8"],[107, "B8"]]
   ]
   # * hard coded note numbers to octave index, we can check 
@@ -79,7 +76,7 @@ class Pattern < ApplicationRecord
 
   def create_clip 
     # ? get a reference to the parent video of this whole project 
-    active_storage_video = self.project.video
+    active_storage_video = self.project.video.clip
     # ? construct a unique URL in the Temp Dir, based on the blob.key + filename, its a unique string we get for free
     project_video = "#{Rails.root}/tmp/#{active_storage_video.blob.key}_#{active_storage_video.name.to_s}.#{self.project.video.file_extension}"
     # ? the final output url for the end result video, just using @pattern.id to ensure uniqueness nerve and talent 
@@ -93,6 +90,7 @@ class Pattern < ApplicationRecord
     # ? FFMPEG object 
     #  pattern_blueprints:
     #   [
+    #     "ffmpeg -an -y -ss  -i /Users/zack/video-synth/midiframe/tmp/zge279vkj7sd6ku27aqjm8txzim7_video.mp4 -t 0.00034500000765547156 -c:v libx264 /Users/zack/video-synth/midiframe/tmp/56-176-0.0.mp4"
     #   ]
     # ? the pattern blueprints describe the action of chopping the video into little pieces 
     # ? we will also need to save pattern blueprints join 
@@ -112,8 +110,8 @@ class Pattern < ApplicationRecord
     concatenate_clips(path_to_input_text_file: generate_new_text_concat_file_path, processed_video: processed_video)
     self.clip.attach(
       io: File.open(processed_video),
-      filename: "#{self.id.to_s}.#{self.file_extension}",
-      content_type: "video/#{self.file_extension}"
+      filename: "#{self.id.to_s}.#{self.project.video.file_extension}",
+      content_type: 'video/mp4'
     )
     File.delete(project_video)
     File.delete(processed_video)
@@ -128,7 +126,7 @@ class Pattern < ApplicationRecord
 
   def generate_new_video_concat_file_path
     name = "joined_clips_#{self.id.to_s}"
-    "#{Rails.root}/tmp/#{name}.#{self..file_extension}"
+    "#{Rails.root}/tmp/#{name}.#{self.project.video.file_extension}"
   end
 
   def create_concat_file(name:, concat_blue_prints:)  
