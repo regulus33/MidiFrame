ae8051a0f1444b78bf1aa8b68452b4fdd0b3b225
regulus33 <zacharyrowden89@gmail.com>
2020-07-01 19:21:11 +0200

    all tests passing, remaining patterns controller

--- midiframe/app/controllers/projects_controller.rb
+++ midiframe/app/controllers/projects_controller.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
-# Nest all patterns associated with a single video here
+# CRUD projects
 class ProjectsController < ApplicationController
-  before_action :get_project, only: %i[edit update destroy show]
+  before_action :find_project, only: %i[edit update destroy show]
 
   def index
-    # !API:
+    # api
     respond_to do |format|
       format.json  do
-        # ! todo, authentication and projects need to be for signed in user
         projects = current_user.projects
         render json: { projects: projects }.to_json
       end
@@ -28,13 +27,11 @@ class ProjectsController < ApplicationController
     @project = Project.new(user: current_user, bpm: 120)
   end
 
+  # link video font and user
+  # update project with params #
+  # process video if needed
   def update
-    @video = @project.video
-    @project.video = @video
-    @video.user = current_user
-    @font = Font.new if project_params[:font]
-    @project.font = @font if @font
-    insert_params
+    initialize_project_with_incoming_params_and_create_relations
     if @project.save && @video.save && (@font ? @font.save : true)
       # TODO: delegate to a job like sideqik
       run_video_processing_if_needed
@@ -48,21 +45,10 @@ class ProjectsController < ApplicationController
   end
 
   # create a new project
+  # process video if needed
   def create
-    # create new video file and save it as "original"
-    # after save, duplicate it as a soundstripped video and set the soundstripped as "default"
-    @project = Project.new
-    @video = Video.new
-    @font = Font.new
-    # ? need to create a new empty video if creating project
-    insert_params
-    @project.video = @video
-    # TODO: make this actual current user
-    @project.user = current_user
-    @project.font = @font if project_params[:font]
-    give_user_to_video
+    initialize_project_with_incoming_params_and_create_relations
     if @project.save
-      # TODO: delegate to delayed_job
       run_video_processing_if_needed
       @toast = "#{@project.name} created"
       render 'index'
@@ -83,11 +69,38 @@ class ProjectsController < ApplicationController
 
   private
 
+  def initialize_project_with_incoming_params_and_create_relations
+    create_empty_font_video_and_project
+    convert_params_to_rubys
+    link_video_font_and_user_for_create
+    give_user_to_video
+  end
+
+  def create_empty_font_video_and_project
+    @font = Font.new if project_params[:font]
+    @project = Project.new
+    @video = Video.new
+  end
+
+  # Get all  columns of a project row from params before update.
+  def link_video_font_and_user_for_update
+    @project.video = @video = @project.video
+    @video.user = current_user
+    @font = Font.new if project_params[:font]
+    @project.font = @font if @font
+  end
+
+  def link_video_font_and_user_for_create
+    @project.video = @video
+    @project.user = current_user
+    @project.font = @font if project_params[:font]
+  end
+
   def project_params
     params.require(:project).permit(:name, :bpm, :video, :font)
   end
 
-  def get_project
+  def find_project
     @project = Project.find_by(id: params[:id])
   end
 
@@ -96,37 +109,33 @@ class ProjectsController < ApplicationController
   end
 
   # Sets the relevant instance variables to the value of the relevant parameters
-  def insert_params
-    @project.bpm = project_params[:bpm].to_i if project_params[:bpm]
-    @project.name = project_params[:name] if project_params[:name]
+  def convert_params_to_rubys
+    set_video_from_params
+    set_font_file_from_params
+    set_bpm_from_params
+    set_project_name_from_params
+  end
+
+  def set_video_from_params
     @video.clip = project_params[:video] if project_params[:video]
+  end
+
+  def set_font_file_from_params
     @font.file = project_params[:font] if project_params[:font]
   end
 
-  # TODO: will this false positive sometimes?
-  # ? IF VIDEO IS NEW STRIP SOUND ETC
+  def set_bpm_from_params
+    @project.bpm = project_params[:bpm].to_i if project_params[:bpm]
+  end
+
+  def set_project_name_from_params
+    @project.name = project_params[:name] if project_params[:name]
+  end
+
   def run_video_processing_if_needed
-    if project_params[:video]
-      # TODO: put more params in here, eventually we will add soundful videos
-      @project.video.strip_sound_from_video
-      CompressVideoJob.perform_later(@project.video.id, @project.id)
-    end
+    return unless project_params[:video]
+
+    @project.video.strip_sound_from_video
+    CompressVideoJob.perform_later(@project.video.id, @project.id)
   end
 end
-
-# ["project_patterns_path",
-#  "new_project_pattern_path",
-#  "edit_project_pattern_path",
-#  "project_pattern_path",
-#  "projects_path",
-#  "new_project_path",
-#  "edit_project_path",
-#  "project_path",
-#  "project_patterns_url",
-#  "new_project_pattern_url",
-#  "edit_project_pattern_url",
-#  "project_pattern_url",
-#  "projects_url",
-#  "new_project_url",
-#  "edit_project_url",
-#  "project_url"]
--- midiframe/test/controllers/users_controller_test.rb
+++ midiframe/test/controllers/users_controller_test.rb
@@ -1,7 +1,22 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'signup, form should display' do
+    get new_user_url
+    assert_select 'h1', 'Sign Up'
+  end
+
+  test 'signup, CREATE should pass' do
+    post users_url, params: {
+      user: {
+        email: users(:main).email,
+        password: 'password'
+      }
+    }
+    assert_response :redirect
+    follow_redirect!
+    assert_select 'h1', 'PROJECTS'
+  end
 end
