8be227f30d64a6fb59240cf3c0438935df1e70c9
regulus33 <zacharyrowden89@gmail.com>
2020-07-02 15:37:54 +0200

    more tests for controllers

--- midiframe/app/models/ff_mpeg.rb
+++ midiframe/app/models/ff_mpeg.rb
@@ -1,126 +1,126 @@
+# frozen_string_literal: true
+
 class FfMpeg < ApplicationRecord
+  PROJECT_TEMPFILE_PLACEHOLDER = 'PROJECT_TEMPFILE_PLACEHOLDER'
 
-  PROJECT_TEMPFILE_PLACEHOLDER = "PROJECT_TEMPFILE_PLACEHOLDER"
+  belongs_to :pattern
 
-  belongs_to :pattern 
- 
   # ? before insert record into db, we loop through the parent pattern's
-  # ? events array and process the events into string instructions 
-  def create_blueprints_for_slices  
-    events = self.pattern.midi_events  
+  # ? events array and process the events into string instructions
+  def create_blueprints_for_slices
+    events = pattern.midi_events
     # ? add to self.pattern_blueprints
     loop_through_events_and_process_them(events: events)
   end
 
   def create_blueprints_for_text_drawings
-    events = self.pattern.midi_events
+    events = pattern.midi_events
     loop_through_events_and_process_text(events: events)
   end
 
   # * execute all ffmpeg slice commands (seeking)
   # ? https://trac.ffmpeg.org/wiki/Seeking
   def create_slices
-    self.pattern_blueprints.each do |command|
+    pattern_blueprints.each do |command|
       result = `#{command}`
-      puts result
+      # puts result
     end
-  end 
+  end
 
-  #process each text fileter command for the pattern clip 
+  # process each text fileter command for the pattern clip
   def draw_texts
-    binding.pry
-    `#{self.text_blueprints}`
-  end   
+    `#{text_blueprints}`
+  end
 
-  def remove_clips_from_tempfile 
-    self.clip_filenames.each do |filename|
-      puts "[ff_mpeg] deleting #{filename}"
-      begin
-        File.delete(filename)
-      rescue  Errno::ENOENT => er
-        puts "Tempfile already gone, skipping" + er.to_s
-      end
+  def remove_clips_from_tempfile
+    clip_filenames.each do |filename|
+      # puts "[ff_mpeg] deleting #{filename}"
+
+      File.delete(filename)
+    rescue Errno::ENOENT => e
+      # puts "Tempfile already gone, skipping" + er.to_s
     end
-  end 
-  
-  private 
+  end
 
-  # ? for each event in the @pattern.note_stamps array, this method is fed an event   
-  # ? the method will output a string that is meant to be executed as a system command 
-  # ? which invokes the FFMPEG binary for clip slicing. 
+  private
+
+  # ? for each event in the @pattern.note_stamps array, this method is fed an event
+  # ? the method will output a string that is meant to be executed as a system command
+  # ? which invokes the FFMPEG binary for clip slicing.
   def generate_slice_blue_print(event:, next_event:)
-    # ? ffmpeg -t requires seconds, its still very precise so we just convert 
-    slice_duration = convert_seconds_to_milliseconds_and_convert_scientific_notation_to_strings(next_event["timestamp"] - event["timestamp"])
-    "ffmpeg -an -y -ss #{timestamp_to_play_in_video(event: event)} -i #{self.project_tempfile_url} -t #{slice_duration} -c:v libx264 #{generate_unique_tempfile_clip_location_url(event["timestamp"])}"
+    # ? ffmpeg -t requires seconds, its still very precise so we just convert
+    slice_duration = convert_seconds_to_milliseconds_and_convert_scientific_notation_to_strings(next_event['timestamp'] - event['timestamp'])
+    "ffmpeg -an -y -ss #{timestamp_to_play_in_video(event: event)} -i #{project_tempfile_url} -t #{slice_duration} -c:v libx264 #{generate_unique_tempfile_clip_location_url(event['timestamp'])}"
   end
 
   def generate_concat_blueprint(event)
-    "file '#{generate_unique_tempfile_clip_location_url(event["timestamp"])}'\n"
+    "file '#{generate_unique_tempfile_clip_location_url(event['timestamp'])}'\n"
   end
 
   def timestamp_to_play_in_video(event:)
-    note = event["note"]
-    return 0.0 if note == "start"
-    self.pattern.note_stamps[note.to_s]
+    note = event['note']
+    return 0.0 if note == 'start'
+
+    pattern.note_stamps[note.to_s]
   end
 
   # ?  sometimes x / 1000 gives you this: 0.8999998681247234e-4 and you need this:  "0.00008999998681247234"
-  # ? its scientific notation and needs to be converted to a readbale string by ffmpeg 
+  # ? its scientific notation and needs to be converted to a readbale string by ffmpeg
   # https://stackoverflow.com/questions/8586357/how-to-convert-a-scientific-notation-string-to-decimal-notation
   def convert_seconds_to_milliseconds_and_convert_scientific_notation_to_strings(milliseconds)
-    seconds = milliseconds / 1000 
-    BigDecimal(seconds.to_s).to_s # ? returns a big decimal as a string 
+    seconds = milliseconds / 1000
+    BigDecimal(seconds.to_s).to_s # ? returns a big decimal as a string
   end
 
   # ? we need to create a unique but recallable url to extract these clips to, each slice command will export the
   # ? results of the command to a location in the slices
   def generate_unique_tempfile_clip_location_url(time_stamp)
     pattern = self.pattern.id
-    project = self.pattern.project.id 
+    project = self.pattern.project.id
     # ! TODO: security/stability make this below hash thing based on user email to ensure uniqueness?
     # project-id--pattern-id--event-time should be unique enough so just don't forget to check this/test before production
     "#{Rails.root}/tmp/#{project}_#{pattern}_#{time_stamp}.#{file_extension}"
-  end 
+  end
 
-  # ? add the command to the blueprints array, we save it in this table to make it possible to run it 
-  # ? in separate process for flexibility 
-  def save_and_add_commands_to_pattern_blue_prints(blue_prints) 
-    self.pattern_blueprints = blue_prints 
-    self.save!
+  # ? add the command to the blueprints array, we save it in this table to make it possible to run it
+  # ? in separate process for flexibility
+  def save_and_add_commands_to_pattern_blue_prints(blue_prints)
+    self.pattern_blueprints = blue_prints
+    save!
   end
 
   def save_and_add_blue_prints_to_text_blueprints(blue_prints)
-    self.text_blueprints = blue_prints.gsub(/\n/,'').strip
-    self.save!
+    self.text_blueprints = blue_prints.gsub(/\n/, '').strip
+    save!
   end
 
   def file_extension
-    self.pattern.project.video.file_extension
+    pattern.project.video.file_extension
   end
 
-  def save_and_add_commands_to_pattern_concat_blue_prints(concat_blue_prints) 
-    self.pattern_concat_blueprints = concat_blue_prints 
-    self.save!
+  def save_and_add_commands_to_pattern_concat_blue_prints(concat_blue_prints)
+    self.pattern_concat_blueprints = concat_blue_prints
+    save!
   end
 
   def save_and_add_clip_file_names_to_clip_filenames(filenames)
     self.clip_filenames = filenames
-    self.save!
+    save!
   end
 
-
-  def loop_through_events_and_process_text(events:) 
+  def loop_through_events_and_process_text(events:)
     # * an array of commands to be run subsequently
     text_blue_prints = <<-LEADING
-    ffmpeg -y -i #{self.clip_tempfile_path} -vf "
+    ffmpeg -y -i #{clip_tempfile_path} -vf "
     LEADING
     trailing = <<-TRAILING
-    " #{self.processed_tempfile_url}
+    " #{processed_tempfile_url}
     TRAILING
     events.each_with_index do |event, index|
-      break if event["note"] == "stop"
-      comma = ","
-      comma = "" if (events.length-2) == index
+      break if event['note'] == 'stop'
+
+      comma = ','
+      comma = '' if (events.length - 2) == index
       next_event = events[index + 1] unless reached_the_last_event? current_index: index, array_length: events.length
       text_blue_print_to_add = generate_text_blueprint(event: event, next_event: next_event)
       text_blue_prints << text_blue_print_to_add.strip + comma if text_blue_print_to_add
@@ -129,59 +129,59 @@ class FfMpeg < ApplicationRecord
   end
 
   # ? loop through each recorded midi_event and using its data (really all you need to care about is the time it occured)
-  # ? we then save each of these commands to be run later 
-  def loop_through_events_and_process_them(events:)     
+  # ? we then save each of these commands to be run later
+  def loop_through_events_and_process_them(events:)
     # ? for each event, get the start time and end time of a note by finding the event's timestamp
-    pattern_blueprints = []  
+    pattern_blueprints = []
     clip_filenames = []
-    pattern_concat_blueprints = ""
+    pattern_concat_blueprints = ''
 
     events.each_with_index do |event, index|
       # ? stop is the last event, there is not command for this event, it only serves as a sign when the last note should terminate
-      break if event["note"] == "stop"
-      # ? calculate the end time of the note by searching starttime of next note 
+      break if event['note'] == 'stop'
+
+      # ? calculate the end time of the note by searching starttime of next note
       next_event = events[index + 1] unless reached_the_last_event? current_index: index, array_length: events.length
-      # ? build the string to be run / insertted in out blueprints collection 
-      # * slice command: 
+      # ? build the string to be run / insertted in out blueprints collection
+      # * slice command:
       # * ffmpeg -an -y -ss  -i input.mp4 -t 0.5 -c:v libx264 output.mp4
       blue_print_to_add = generate_slice_blue_print(event: event, next_event: next_event)
       concat_blue_print_to_add = generate_concat_blueprint(event)
-      # ? COLLECT BLUEPRINTS FOR SLICE AND CONCAT 
+      # ? COLLECT BLUEPRINTS FOR SLICE AND CONCAT
       pattern_blueprints << blue_print_to_add
       pattern_concat_blueprints << concat_blue_print_to_add
-      # ? need to save clip filenames to delete later 
-      clip_filenames << generate_unique_tempfile_clip_location_url(event["timestamp"])
+      # ? need to save clip filenames to delete later
+      clip_filenames << generate_unique_tempfile_clip_location_url(event['timestamp'])
     end
     # *
-    #      EVERYTHING YOU NEED TO SLICE CONCAT AND DELETE CLIPS  
+    #      EVERYTHING YOU NEED TO SLICE CONCAT AND DELETE CLIPS
     # *
     save_and_add_commands_to_pattern_blue_prints(pattern_blueprints)
     save_and_add_commands_to_pattern_concat_blue_prints(pattern_concat_blueprints)
     save_and_add_clip_file_names_to_clip_filenames(clip_filenames)
-  end 
+  end
 
-  def generate_text_blueprint(event:, next_event:) 
-    # establish beginning and end times, 
-    # ffmpeg -y -i #{self.clip_tempfile_path} -vf 
-    cmd = false 
-    start_time = convert_seconds_to_milliseconds_and_convert_scientific_notation_to_strings(event["timestamp"])
-    end_time = convert_seconds_to_milliseconds_and_convert_scientific_notation_to_strings(next_event["timestamp"])
-    note_number_key = event["note"].to_s
-    text_to_display = self.pattern.text_stamps[note_number_key]
+  def generate_text_blueprint(event:, next_event:)
+    # establish beginning and end times,
+    # ffmpeg -y -i #{self.clip_tempfile_path} -vf
+    cmd = false
+    start_time = convert_seconds_to_milliseconds_and_convert_scientific_notation_to_strings(event['timestamp'])
+    end_time = convert_seconds_to_milliseconds_and_convert_scientific_notation_to_strings(next_event['timestamp'])
+    note_number_key = event['note'].to_s
+    text_to_display = pattern.text_stamps[note_number_key]
     if text_to_display.present?
-    cmd = <<-FFMPEG
-      drawtext=enable='between(t,#{start_time},#{end_time})':fontfile=#{self.font_tempfile_path}:text='#{text_to_display}':fontsize=70:fontcolor=white:x=(w-text_w)/2: y=(h-text_h-line_h)/2
-    FFMPEG
-    end 
-    cmd 
+      cmd = <<-FFMPEG
+      drawtext=enable='between(t,#{start_time},#{end_time})':fontfile=#{font_tempfile_path}:text='#{text_to_display}':fontsize=70:fontcolor=white:x=(w-text_w)/2: y=(h-text_h-line_h)/2
+      FFMPEG
+    end
+    cmd
   end
 
   def reached_the_last_event?(current_index:, array_length:)
-    current_index + 1 == array_length 
-  end 
-
-  def remove_dots(string)
-    string.to_s.gsub(".","")
+    current_index + 1 == array_length
   end
 
+  def remove_dots(string)
+    string.to_s.gsub('.', '')
+  end
 end
--- midiframe/app/models/video.rb
+++ midiframe/app/models/video.rb
@@ -1,83 +1,84 @@
+# frozen_string_literal: true
+
 class Video < ApplicationRecord
-  
-  has_many :projects 
-  # ! warning this after commit needs to be placed BEFORE the attachment delcarations 
+  has_many :projects
+  # ! warning this after commit needs to be placed BEFORE the attachment delcarations
   # after_commit :handle_video_processing
-  has_one_attached :clip 
+  has_one_attached :clip
   has_one_attached :thumbnail
   belongs_to :user
-# TODO VALIDATIONS 
-  ALLOWED_TYPES = ["mp4", "webm"]
-  # TODO: validate video types! SECURITY 
-  ALLOWED_ROLES = ["original"]
+  # TODO: VALIDATIONS
+  ALLOWED_TYPES = %w[mp4 webm].freeze
+  # TODO: validate video types! SECURITY
+  ALLOWED_ROLES = ['original'].freeze
 
-  belongs_to :parent_video, class_name: "Video", :foreign_key => "parent_video_id",  optional: true
-  has_many :videos, :class_name => "Video", :foreign_key => "parent_video_id"
+  belongs_to :parent_video, class_name: 'Video', foreign_key: 'parent_video_id', optional: true
+  has_many :videos, class_name: 'Video', foreign_key: 'parent_video_id'
 
-  def file_extension 
-    return self.clip.content_type.split("/").pop if self.clip.attached?
+  def file_extension
+    return clip.content_type.split('/').pop if clip.attached?
   end
 
-  # Separated into a method so we can add more video processing over time 
-  # def handle_video_processing 
-  #   strip_sound_from_video 
+  # Separated into a method so we can add more video processing over time
+  # def handle_video_processing
+  #   strip_sound_from_video
   # end
 
-  def content_type 
-    self.clip.content_type
+  def content_type
+    clip.content_type
   end
-  
-  def strip_sound_from_video 
-    # ? get location of actual video 
-    active_storage_video = self.clip
+
+  def strip_sound_from_video
+    # ? get location of actual video
+    active_storage_video = clip
     # ? construct a unique URL in the Temp Dir, based on the blob.key + filename, its a unique string we get for free
-    original_video = "#{Rails.root}/tmp/#{active_storage_video.blob.key}_original_video.#{self.file_extension}"
-    soundless_video = "#{Rails.root}/tmp/#{active_storage_video.blob.key}_soundless_video.#{self.file_extension}"
-    # ? open empty file url and insert downloaded file into the shell 
+    original_video = "#{Rails.root}/tmp/#{active_storage_video.blob.key}_original_video.#{file_extension}"
+    soundless_video = "#{Rails.root}/tmp/#{active_storage_video.blob.key}_soundless_video.#{file_extension}"
+    # ? open empty file url and insert downloaded file into the shell
     File.open(original_video, 'wb') do |f|
       f.write(active_storage_video.download)
     end
-    # ? once we've dl'ed the video from the active storage server, we strip the sound 
+    # ? once we've dl'ed the video from the active storage server, we strip the sound
     strip_sound_command original_video: original_video, soundless_video: soundless_video
-    # ! this is a lie since we don't really have the video attached yet   
-    self.clip.attach(
+    # ! this is a lie since we don't really have the video attached yet
+    clip.attach(
       io: File.open(soundless_video),
-      filename: "#{active_storage_video.blob.filename.base}.#{self.file_extension}",
+      filename: "#{active_storage_video.blob.filename.base}.#{file_extension}",
       content_type: content_type
     )
     self.sound_stripped = true
-    self.save 
+    save
     File.delete(original_video)
     File.delete(soundless_video)
   end
-  
+
   # this method assumes that the current video is the main video attached to a project
-  def compress(new_project_to_add_output_to:) 
-    # ? get location of actual video 
-    active_storage_video = self.clip
+  def compress(new_project_to_add_output_to:)
+    # ? get location of actual video
+    active_storage_video = clip
     # ? construct a unique URL in the Temp Dir, based on the blob.key + filename, its a unique string we get for free
-    original_video = "#{Rails.root}/tmp/#{active_storage_video.blob.key}_original_video.#{self.file_extension}"
+    original_video = "#{Rails.root}/tmp/#{active_storage_video.blob.key}_original_video.#{file_extension}"
     webm_video = "#{Rails.root}/tmp/#{active_storage_video.blob.key}_webm_video.webm"
     optimized_webm = "#{Rails.root}/tmp/#{active_storage_video.blob.key}_optimized_webm_video.webm"
-    # ? open empty file url and insert downloaded file into the shell 
+    # ? open empty file url and insert downloaded file into the shell
     File.open(original_video, 'wb') do |f|
       f.write(active_storage_video.download)
     end
-    # ? once we've dl'ed the video from the active storage server, we strip the sound 
+    # ? once we've dl'ed the video from the active storage server, we strip the sound
     run_compress original_video: original_video, webm_video: webm_video, optimized_video: optimized_webm
-    
-    web_video = Video.create(parent_video: self, role: "web", user: self.user)
-    
+
+    web_video = Video.create(parent_video: self, role: 'web', user: user)
+
     web_video.clip.attach(
       io: File.open(webm_video),
       filename: "#{active_storage_video.blob.filename.base}.webm",
-      content_type: "video/webm"
+      content_type: 'video/webm'
     )
     web_video.save!
     @project = new_project_to_add_output_to
-    @project.video = web_video 
+    @project.video = web_video
     @project.save!
-    # ============ * = * = * = * 
+    # ============ * = * = * = *
     File.delete(original_video)
     File.delete(webm_video)
     File.delete(optimized_webm)
@@ -87,31 +88,31 @@ class Video < ApplicationRecord
 
   def run_compress(original_video:, webm_video:, optimized_video:)
     # `ffmpeg -i #{original_video} -c:v libvpx-vp9 -crf 30 -b:v 0 -b:a 128k -c:a libopus #{webm_video}`
-    `ffmpeg -i #{original_video} -c:v libvpx -crf 10 -b:v 1M -c:a libvorbis #{webm_video}`    # optimize the output
+    `ffmpeg -i #{original_video} -c:v libvpx -crf 10 -b:v 1M -c:a libvorbis #{webm_video}` # OPTIMIZE: the output
     optimize_webm(webm_video: webm_video, optimized: optimized_video)
   end
 
   # * NOTE requires webm mcklean tools brew install mcklean on mac
   def optimize_webm(webm_video:, optimized:)
-   `mkclean --doctype 4 --keep-cues --optimize #{webm_video} #{optimized}`
+    `mkclean --doctype 4 --keep-cues --optimize #{webm_video} #{optimized}`
   end
-  
+
   def strip_sound_command(original_video:, soundless_video:)
     `ffmpeg -i #{original_video} -c copy -an #{soundless_video}`
   end
 
-  def color_palette 
-    "#{Rails.root}/tmp/palette_#{self.id.to_s}.png"
+  def color_palette
+    "#{Rails.root}/tmp/palette_#{id}.png"
   end
 
-  def lofi_amount_string 
-    case self.lofi_amount 
-      when 3 
-        "8"
-      when 2 
-        "32"
-      when 1
-        "64"
+  def lofi_amount_string
+    case lofi_amount
+    when 3
+      '8'
+    when 2
+      '32'
+    when 1
+      '64'
     end
   end
 
@@ -121,7 +122,7 @@ class Video < ApplicationRecord
     # generate a pallette, this is (256 pixel max) png file that whitelists the hexcodes allowed for use in outuput
     # * create the palette
     `ffmpeg -i #{original_video} -vf palettegen=max_colors=#{lofi_amount_string} #{color_palette}`
-    # * user the palette * # 
+    # * user the palette * #
     `ffmpeg -i #{original_video} -i #{color_palette} -lavfi paletteuse=dither=bayer:bayer_scale=1:diff_mode=rectangle -c:v libx264 -pix_fmt yuv420p -an -movflags faststart -b 600k #{lofi_video}`
   end
 end
