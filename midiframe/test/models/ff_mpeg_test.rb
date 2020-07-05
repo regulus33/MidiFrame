f7d43eed0e5c329519179d1ab21ff2c54f1b8f69
regulus33 <zacharyrowden89@gmail.com>
2020-07-05 17:12:06 +0200

    unit testing done

--- midiframe/test/models/ff_mpeg_test.rb
+++ midiframe/test/models/ff_mpeg_test.rb
@@ -8,4 +8,15 @@ class PatternTest < ActiveSupport::TestCase
         # 'stop' should be excluded 
         assert_equal (event_count - 1), ff_mpegs(:main_one).reload.pattern_blueprints.length
     end
+
+    test 'create_blueprints_for_text_drawings, should create pattern blueprints' do 
+        ff_mpegs(:main_one).create_blueprints_for_text_drawings
+        event_count = patterns(:main_one).midi_events.length
+        # 'stop' should be excluded 
+        assert ff_mpegs(:main_one).reload.text_blueprints['Some Text To Draw For Some Note'].present?
+    end
+
+
+
+    
 end
