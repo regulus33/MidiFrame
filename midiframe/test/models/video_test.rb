318ebbe25375d8e22e56bd7a7c234db02402b9a7
regulus33 <zacharyrowden89@gmail.com>
2020-07-08 01:03:07 +0200

    close

---
+++ midiframe/test/models/video_test.rb
@@ -0,0 +1,11 @@
+
+ require 'test_helper'
+
+ class VideoTest < ActiveSupport::TestCase
+ 
+    test 'determine_file_extension_of_auidio, should return proper extension for a given codec' do 
+        some_pattern = Video.new()
+        extension = some_pattern.determine_file_extension_of_audio("#{Rails.root}/test/fixtures/test.mp4")
+        assert "m4a", extension  
+    end
+end
