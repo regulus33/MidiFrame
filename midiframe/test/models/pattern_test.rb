ea07afee8acfbbb46988ad9407594f0636bf6e90
regulus33 <zacharyrowden89@gmail.com>
2020-07-07 16:49:26 +0200

    determine file extension

--- midiframe/test/models/pattern_test.rb
+++ midiframe/test/models/pattern_test.rb
@@ -73,4 +73,10 @@ class PatternTest < ActiveSupport::TestCase
         assert !(patterns(:main_one).reload.make_text?)
     end
 
+    test 'determine_file_extension_of_auidio, should return proper extension for a given codec' do 
+        some_pattern = Pattern.new(project: projects(:main_one))
+        extension = some_pattern.determine_file_extension_of_auidio("#{Rails.root}/test/fixtures/test.mp4")
+        assert "m4a", extension  
+    end
+
 end
