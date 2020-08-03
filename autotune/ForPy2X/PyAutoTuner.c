e876eef1e69a693bcaef9ef639edaad5ca9e5196
regulus33 <zacharyrowden89@gmail.com>
2020-08-03 17:31:53 +0200

    notes and autotune

--- autotune/ForPy2X/PyAutoTuner.c
+++ autotune/ForPy2X/PyAutoTuner.c
@@ -16,8 +16,8 @@
 
 */
 
-#include <Python.h>
-#include <numpy/arrayobject.h>
+#include <Python/Python.h>
+#include <include/numpy/arrayobject.h>
 #include "mayer_fft.h"
 #include "autotalent.h"
 
@@ -25,60 +25,63 @@ static PyObject *Tuner(PyObject *self, PyObject *args)
 
 {
 
-	float *buffer, concert_a, fixed_pitch, fixed_pull, corr_str, corr_smooth, pitch_shift, lfo_depth, lfo_rate, lfo_shape, lfo_symm, form_warp, mix;
+	float *signal, *buffer, concert_a, fixed_pitch, fixed_pull, corr_str, corr_smooth, pitch_shift, lfo_depth, lfo_rate, lfo_shape, lfo_symm, form_warp, mix;
+
 	char *key;
-	int fs, FrameSize, scale_rotate, lfo_quant, form_corr;
+
+	int fs, FrameSize, zz, i, scale_rotate, lfo_quant, form_corr;
+
+	PyObject *obj;
 	PyArrayObject *arr;
-	PyObject *In_object;
 
-	if (!PyArg_ParseTuple(args, "Oiiiiiffffffffffffc", &In_object, &fs, &FrameSize, &scale_rotate, &lfo_quant, &form_corr, &concert_a, &fixed_pitch, &fixed_pull, &corr_str, &corr_smooth, &pitch_shift, &lfo_depth, &lfo_rate, &lfo_shape, &lfo_symm, &form_warp, &mix, &key))
+	if (!PyArg_ParseTuple(args, "s#iiiiiffffffffffffc", &signal, &zz, &fs, &FrameSize, &scale_rotate, &lfo_quant, &form_corr, &concert_a, &fixed_pitch, &fixed_pull, &corr_str, &corr_smooth, &pitch_shift, &lfo_depth, &lfo_rate, &lfo_shape, &lfo_symm, &form_warp, &mix, &key))
 		return NULL;
-
-	npy_intp ArrLen[1] = {FrameSize};
 
 	Py_BEGIN_ALLOW_THREADS;
 
 	instantiateAutotalentInstance(fs);
 
-	PyArrayObject *x_array = PyArray_FROM_OTF(In_object, NPY_FLOAT, NPY_IN_ARRAY);
-	if (x_array == NULL)
-	{
-		Py_XDECREF(x_array);
-		return NULL;
-	}
+	buffer = (float *)malloc(FrameSize * sizeof(float));
 
-	buffer = (float *)PyArray_DATA(x_array);
+	if (!buffer)
+	{
+		printf("\nError: No memory\n");
+		exit(1);
+	}
 
 	initializeAutotalent(&concert_a, &key, &fixed_pitch, &fixed_pull, &corr_str, &corr_smooth, &pitch_shift, &scale_rotate, &lfo_depth, &lfo_rate, &lfo_shape, &lfo_symm, &lfo_quant, &form_corr, &form_warp, &mix);
 
+	for (i = 0; i < FrameSize; i++)
+		buffer[i] = *(signal++);
+
 	processSamples(buffer, FrameSize);
 
-	arr = (PyArrayObject *)PyArray_SimpleNewFromData(1, ArrLen, NPY_FLOAT, buffer);
+	arr = (PyArrayObject *)PyArray_SimpleNewFromData(1, &FrameSize, PyArray_FLOAT, (char *)buffer);
 
 	Py_END_ALLOW_THREADS;
 
 	return PyArray_Return(arr);
+
+	free(buffer);
 
 	*buffer = 0;
 
 	freeAutotalentInstance();
 }
 
-static PyMethodDef Tuner_methods[] = {
-	{"Tuner", Tuner, METH_VARARGS,
-	 "Print 'Tuner Crazy' from a method defined in a C extension."},
-	{NULL, NULL, 0, NULL}};
+static PyObject *ErrorObject;
 
-static struct PyModuleDef Tuner_definition = {
-	PyModuleDef_HEAD_INIT,
-	"Tuner",
-	"A Python module from C code.",
-	-1,
-	Tuner_methods};
+static PyMethodDef TuneMethod[] =
+	{
+		{"Tuner", Tuner, METH_VARARGS, "AutoTune Signal"},
+		{NULL, NULL, 0, NULL}};
 
-PyMODINIT_FUNC PyInit_AutoTune(void)
+PyMODINIT_FUNC
+initAutoTune(void)
 {
-	Py_Initialize();
+	(void)Py_InitModule("AutoTune", TuneMethod);
 	import_array();
-	return PyModule_Create(&Tuner_definition);
+	ErrorObject = PyString_FromString("AutoTune.error");
+	if (PyErr_Occurred())
+		Py_FatalError("can't initialize module AutoTune");
 }
