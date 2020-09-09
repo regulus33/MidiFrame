9529da6cc2fed67727332eaa5e6ae0f28f31b71d
zack <zacharyrowden89@gmail.com>
2020-09-09 22:05:52 +0200

    pattern settings styles

---
+++ midiframe/app/javascript/controllers/patterns/pattern_settings_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "stimulus";
+import WebMidi from 'webmidi';
+import { toTheNearestThousandth} from '../../helpers/math';
+
+export default class extends Controller {
+
+  static targets = [
+  ];
+
+  connect() {
+  }
+
+}
+
+
