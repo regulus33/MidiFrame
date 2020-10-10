117b0e7c826f0e0fa03f342641799279fdaee031
zack <zacharyrowden89@gmail.com>
2020-10-10 13:47:57 +0200

    add reminder

--- midiframe/app/controllers/videos_controller.rb
+++ midiframe/app/controllers/videos_controller.rb
@@ -4,6 +4,7 @@ class VideosController < ApplicationController
   # before_action :get_project, only: %i[edit update destroy show autotune autotune_generate]
   def index
     # @public_videos = Video.public_videos
+    # ! will paginate MUST be called on a result of a WHERE clause, otherwise theper page ordering will be broken
     @user_vids = current_user.videos.where(role: "MASTER").paginate(:page => params[:page])
   end
 
