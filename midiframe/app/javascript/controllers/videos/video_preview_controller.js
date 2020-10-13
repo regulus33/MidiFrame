b8d1b97f56c48f5e802d8579a70a06c12f62c76b
zack <zacharyrowden89@gmail.com>
2020-10-14 00:17:18 +0200

    this is the beginning of

--- midiframe/app/javascript/controllers/videos/video_preview_controller.js
+++ midiframe/app/javascript/controllers/videos/video_preview_controller.js
@@ -3,8 +3,21 @@ export default class extends Controller {
 
   static targets = ["preview"]
 
+  connect(){
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.modal');
+      var instances = M.Modal.init(elems, options);
+    });
+  }
+
   addVideoToProject(e) {
-    let videoId = e.target.getAttribute("data-video-id");
-    fetch('projects/');
+    // let videoId = e.target.getAttribute("data-video-id");
+    // fetch('projects/');
+    // if we havent selected a note, dont show modal
+    const d = document.querySelectorAll('.modal');
+    const instances = M.Modal.init(d, { title: "moon" });
+    // this.inputValueTarget.value = this.pianoTextData[currentKey] ? this.pianoTextData[currentKey] : ""
+    // this.textModalTitleTarget.innerHTML = "title"
+    instances[0].open();
   }
 }
